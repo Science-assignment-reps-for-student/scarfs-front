97064b83adfdd555cb833a0a6e9e8b76ecbe0d63
이성진 <chlskatnr1@naver.com>
2020-07-11 19:01:29 +0900

    [CREATE] components, container index.js
    2020.07.11

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Admin from './Admin/Admin';
+import AdminHeader from './AdminHeader';
+import AdminLogin from './AdminLogin';
+
+export { Admin, AdminHeader, AdminLogin };
---
+++ src/containers/index.js
@@ -0,0 +1,5 @@
+import AdminContainer from './Admin/Admin';
+import AdminLoginContainer from './AdminLogin/AdminLogin';
+import AdminHeaderContainer from './AdminHeader/AdminHeader';
+
+export { AdminContainer, AdminHeaderContainer, AdminLoginContainer };
