f32f58ec9bd65fc745a96128a65008f348544739
이성진 <chlskatnr1@naver.com>
2020-07-11 18:58:56 +0900

    [CREATE] AdminHeader container
    2020.07.11

---
+++ src/containers/AdminHeader/AdminHeader.tsx
@@ -0,0 +1,10 @@
+import React, { FC, ReactElement } from 'react';
+import { AdminHeader } from '../../components';
+
+interface Props { }
+
+const AdminHeaderContainer: FC<Props> = (): ReactElement => {
+  return <AdminHeader />;
+};
+
+export default AdminHeaderContainer;
