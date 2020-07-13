c8543318994e281b3d09a70d3a760f5a8f9eb1ae
ohjunsang <pandati@dsm.hs.kr>
2020-07-13 20:01:12 +0900

    [FIX] main style fix

--- src/style/Main/index.ts
+++ src/style/Main/index.ts
@@ -1,11 +1,31 @@
 import styled from 'styled-components';
-import { megaphone, calender } from '../../asset/Main';
+import { megaphone, calender, logo, logout, background } from '../../asset/Main';
+
+export const Background = styled.div`
+  width: 100%;
+  height: 152px;
+  background-image: url(${background});
+`;
 
 export const Body = styled.div`
   width: 100%;
-  height: calc(100vh - 60px);
+  min-width: 1342px;
   padding: 0px 200px 0px 200px;
   box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Wrapper = styled.div<{ margin: number }>`
+  margin-top: ${props => props.margin}px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-end;
+`;
+
+export const TaskListWrapper = styled.div`
+  height: 600px;
+  align-self: flex-end;
 `;
 
 export const TaskList = styled.div`
@@ -13,6 +33,7 @@ export const TaskList = styled.div`
   height: 190px;
   display: flex;
   flex-wrap: wrap;
+  justify-content: flex-end;
 `;
 
 export const TaskListComponent = styled.div`
@@ -57,6 +78,7 @@ export const TaskListHeader = styled.div`
   align-items: center;
   box-sizing: border-box;
   color: white;
+  margin-top: 80px;
   > div {
     width: 20px;
     height: 20px;
@@ -66,5 +88,121 @@ export const TaskListHeader = styled.div`
   }
   > .megaphone {
     background-image: url(${megaphone});
+  }
+`;
+
+export const Logo = styled.div`
+  width: 500px;
+  height: 380px;
+`;
+
+export const LogoTitle = styled.p`
+  width: 380px;
+  font-size: 1.625rem;
+  text-align: right;
+  > span.point {
+    color: #505bff;
+  }
+  font-weight: 500;
+`;
+
+export const LogoSubTitle = styled(LogoTitle)`
+  font-weight: 300;
+  text-align: left;
+`;
+
+export const LogoImg = styled.div`
+  background-image: url(${logo});
+  width: 100%;
+  height: 263px;
+`;
+
+export const LogoText = styled.p`
+  margin-top: 30px;
+  font-size: 0.6875rem;
+  font-weight: 400;
+  letter-spacing: 5px;
+  text-align: right;
+`;
+
+export const LogoTitleWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  flex-wrap: wrap;
+`;
+
+export const UserMain = styled.div`
+  display: flex;
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 132px;
+  align-self: flex-end;
+  > div {
+    width: 365px;
+    height: 100px;
+    display: flex;
+  }
+`;
+
+export const UserInfo = styled.div`
+  width: 250px;
+  height: 100%;
+  padding: 14px 29px;
+  box-sizing: border-box;
+  box-shadow: 2px 2px 1px 1px #d3d3d3;
+  border: 1px solid #d3d3d3;
+`;
+
+export const UserInfoName = styled.div`
+  font-size: 1.25rem;
+  font-weight: 600;
+  text-align: right;
+  margin-bottom: 16px;
+`;
+
+export const UserInfoTask = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 16px;
+`;
+
+export const UserInfoTaskDetail = styled.div`
+  > p {
+    margin: 1px;
+  }
+  > .remain {
+    color: #ff5700;
+  }
+  > .complete {
+    color: #505bff;
+  }
+`;
+
+export const UserInfoButton = styled.div`
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  background-color: #505bff;
+  margin-left: 15px;
+  box-shadow: 2px 2px 1px 1px #d3d3d3;
+  cursor: pointer;
+  > div {
+    > div {
+      width: 30px;
+      height: 30px;
+      margin: 0 auto;
+      background-image: url(${logout});
+      margin-bottom: 5px;
+    }
+    > p {
+      font-size: 0.75rem;
+      color: white;
+      font-weight: 500;
+    }
   }
 `;
