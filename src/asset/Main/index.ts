f62a9f9776b161244d371818e697edb1e4d0639f
ohjunsang <pandati@dsm.hs.kr>
2020-07-13 20:01:01 +0900

    [FIX] assets/Main index.ts

--- src/asset/Main/index.ts
+++ src/asset/Main/index.ts
@@ -1,3 +1,5 @@
 export { default as calender } from './icon_calender.png';
 export { default as megaphone } from './icon_megaphone.png';
 export { default as logo } from './logo.png';
+export { default as logout } from './icon_logout.png';
+export { default as background } from './background.png';
