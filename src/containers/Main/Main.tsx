a62bb28e9b38657db97b7dc5cdd21811d6876436
ohjunsang <pandati@dsm.hs.kr>
2020-07-13 20:04:23 +0900

    [FIX] 쓸모없는 코드 삭제

--- src/containers/Main/Main.tsx
+++ src/containers/Main/Main.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react';
 import { LoginedMain } from '../../components/Main';
-import * as S from '../../style/Main';
 
 const Main: FC = () => {
   return <LoginedMain />;
