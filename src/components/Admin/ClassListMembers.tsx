312eae2c714d60afb45d957145127b8f52291311
이성진 <chlskatnr1@naver.com>
2020-07-13 13:48:20 +0900

    [MODIFY] ClassListMember, ClassListReport
    2020.07.13

--- src/components/Admin/ClassListMembers.tsx
+++ src/components/Admin/ClassListMembers.tsx
@@ -1,7 +1,7 @@
 import React, { FC, ReactElement } from 'react';
 import * as S from './style';
-import adminO from '../../assets/Admin/adminO.svg';
-import adminX from '../../assets/Admin/adminX.svg';
+import ClassListCommon from './ClassListCommon';
+import ClassListHeadCommon from './ClassListHeadCommon';
 
 interface Props {
   onlyPersonal: boolean;
@@ -10,27 +10,15 @@ interface Props {
 const ClassListMembers: FC<Props> = ({ onlyPersonal }): ReactElement => {
   return (
     <S.SubjectClsContentMembers>
-      {onlyPersonal && <h5>동료평가</h5>}
-      <ul>
-        <li>
-          <span>학번</span>
-          <span>이름</span>
-          <span>제출여부</span>
-        </li>
+      {onlyPersonal && <S.SubjectClsContentCommonTitle>동료평가</S.SubjectClsContentCommonTitle>}
+      <S.SubjectClsContentCommonList>
+        <ClassListHeadCommon isReport={false} />
         {Array(20)
           .fill(0)
-          .map((_, i) => {
-            return (
-              <li>
-                <span>120{i + 1}</span>
-                <span>이름</span>
-                <span>
-                  <img src={i % 2 === 0 ? adminO : adminX} alt="notComplete" title="notComplete" />
-                </span>
-              </li>
-            );
-          })}
-      </ul>
+          .map((_, i) => (
+            <ClassListCommon key={i} i={i} isReport={false} />
+          ))}
+      </S.SubjectClsContentCommonList>
     </S.SubjectClsContentMembers>
   );
 };
--- src/components/Admin/ClassListReport.tsx
+++ src/components/Admin/ClassListReport.tsx
@@ -1,7 +1,7 @@
 import React, { FC, ReactElement } from 'react';
 import * as S from './style';
-import adminO from '../../assets/Admin/adminO.svg';
-import adminX from '../../assets/Admin/adminX.svg';
+import ClassListCommon from './ClassListCommon';
+import ClassListHeadCommon from './ClassListHeadCommon';
 
 interface Props {
   onlyPersonal: boolean;
@@ -12,31 +12,15 @@ const ClassListReport: FC<Props> = ({ onlyPersonal }): ReactElement => {
     <S.SubjectClsContentReport>
       {onlyPersonal && (
         <>
-          <h5>팀보고서</h5>
-          <ul>
-            <li>
-              <span>팀이름</span>
-              <span>학번</span>
-              <span>이름</span>
-              <span>제출여부</span>
-            </li>
+          <S.SubjectClsContentCommonTitle>팀보고서</S.SubjectClsContentCommonTitle>
+          <S.SubjectClsContentCommonList>
+            <ClassListHeadCommon isReport={true} />
             {Array(20)
               .fill(0)
-              .map((_, i) => {
-                return (
-                  <li key={i}>
-                    <span>{i % 4 === 0 && '팀이름'}</span>
-                    <span>1201</span>
-                    <span>이름</span>
-                    <span>
-                      {i % 4 === 0 && (
-                        <img src={i % 8 === 0 ? adminO : adminX} alt="complete" title="complete" />
-                      )}
-                    </span>
-                  </li>
-                );
-              })}
-          </ul>
+              .map((_, i) => (
+                <ClassListCommon key={i} i={i} isReport={true} />
+              ))}
+          </S.SubjectClsContentCommonList>
         </>
       )}
     </S.SubjectClsContentReport>
