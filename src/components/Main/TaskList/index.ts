e654f0def1e9efe40d6cf2b7fa48756f6ceb6293
ohjunsang <pandati@dsm.hs.kr>
2020-07-14 18:23:41 +0900

    [DELETE] 중복되는 파일 삭제

--- src/components/Main/TaskList/index.ts
+++
@@ -1,6 +0,0 @@
-import TaskList from './TaskList';
-
-export { default as TaskHeader } from './TaskHeader';
-export { default as TaskButton } from './TaskButton';
-export { default as TaskListComponent } from './TaskListComponent';
-export default TaskList;
