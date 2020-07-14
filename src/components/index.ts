46753d811d8bb1c37e4f8504ca4ae71419973034
이성진 <chlskatnr1@naver.com>
2020-07-14 11:17:54 +0900

    [MODIFY] solve components, container index.ts conflict
    2020.07.14

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import Admin from './Admin/Admin';
 import AdminHeader from './AdminHeader/AdminHeader';
 import AdminLogin from './AdminLogin/AdminLogin';
-
-export { Admin, AdminHeader, AdminLogin };
-=======
 import { NotFound } from './NotFound';
 
-export { NotFound };
->>>>>>> develop
+export { Admin, AdminHeader, AdminLogin, NotFound };
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 import AdminContainer from './Admin/AdminContainer';
 import AdminLoginContainer from './AdminLogin/AdminLoginContainer';
 import AdminHeaderContainer from './AdminHeader/AdminHeaderContainer';
 
 export { AdminContainer, AdminHeaderContainer, AdminLoginContainer };
-=======
 export { HeaderContainer } from './Default';
 export { MainContainer } from './Main';
->>>>>>> develop
