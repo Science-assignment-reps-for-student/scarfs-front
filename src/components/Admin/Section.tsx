d46eaba41575c5370e7e682442447687a5feb051
이성진 <chlskatnr1@naver.com>
2020-07-14 23:07:48 +0900

    [MODIFY] Section
    2020.07.14

--- src/components/Admin/Section.tsx
+++ src/components/Admin/Section.tsx
@@ -1,10 +1,10 @@
-import React, { FC, ReactElement, useEffect } from 'react';
+import React, { FC, ReactElement } from 'react';
 import * as S from './style';
 import Subject from './Subject';
 import { CombineAdminSubjects } from '../../modules/reducer/Admin/admin';
-import { Personal, PersonalSubject } from '../../modules/reducer/Admin/adminPersonal';
-import { Team, TeamSubject } from '../../modules/reducer/Admin/adminTeam';
-import { Experiment, ExperimentSubject } from '../../modules/reducer/Admin/adminExperiment';
+import { Personal } from '../../modules/reducer/Admin/adminPersonal';
+import { Team } from '../../modules/reducer/Admin/adminTeam';
+import { Experiment } from '../../modules/reducer/Admin/adminExperiment';
 import { useSelector } from 'react-redux';
 import { StoreState } from '../../modules/reducer/Admin';
 
@@ -12,6 +12,8 @@ interface Props {}
 
 interface ISubject extends CombineAdminSubjects {
   type?: string;
+  title?: string;
+  classNum?: number;
 }
 
 const AdminSection: FC<Props> = (): ReactElement => {
@@ -19,7 +21,7 @@ const AdminSection: FC<Props> = (): ReactElement => {
     (state: StoreState) => state.admin,
   );
 
-  const sortAllList = (
+  const combineSortAllList = (
     { personal_assignment = [] }: Personal,
     { team_assignment = [] }: Team,
     { experiment_assignment = [] }: Experiment,
@@ -28,17 +30,33 @@ const AdminSection: FC<Props> = (): ReactElement => {
       a.id > b.id ? 1 : -1,
     );
 
+  const combineSortAllList2 = (personal: Personal[], team: Team[]) => {
+    let result = [];
+    let add = [];
+
+    for (let i = 0; i < personal.length; i++) {
+      const one = personal[i].personal_assignment;
+      for (let j = 0; j < personal[i].personal_assignment.length; j++) {
+        const two = one[j];
+        console.log(two);
+      }
+    }
+  };
+
   return (
     <S.AdminSection>
-      {loading ? (
+      {/* {loading ? (
         <div>기다리세여!</div>
       ) : (
-        sortAllList(personalList, teamList, experimentList).map(item => {
+        combineSortAllList(personalList, teamList, experimentList).map(item => {
           const { id, title } = item;
-          console.log(item);
           return <Subject key={id} subject={title} onlyPersonal={false} />;
         })
-      )}
+      )} */}
+      {console.log(personalList, teamList)}
+      {combineSortAllList2(personalList, teamList)}
+
+      <Subject onlyPersonal={true} subject={'title'} />
     </S.AdminSection>
   );
 };
