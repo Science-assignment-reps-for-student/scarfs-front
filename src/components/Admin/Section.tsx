7319a922be1ef887ac00b1bf9157df7c096e88a9
이성진 <chlskatnr1@naver.com>
2020-07-15 16:59:17 +0900

    [MODIFY] section

--- src/components/Admin/Section.tsx
+++ src/components/Admin/Section.tsx
@@ -1,7 +1,8 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useCallback } from 'react';
 import * as S from './style';
 import Subject from './Subject';
-import { CombineAdminSubjects } from '../../modules/reducer/Admin/admin';
+import Class from './Class';
+import { CombineAdminSubjects, CombineAdminSubject } from '../../modules/reducer/Admin/admin';
 import { Personal } from '../../modules/reducer/Admin/adminPersonal';
 import { Team } from '../../modules/reducer/Admin/adminTeam';
 import { Experiment } from '../../modules/reducer/Admin/adminExperiment';
@@ -9,56 +10,57 @@ import { useSelector } from 'react-redux';
 import { StoreState } from '../../modules/reducer/Admin';
 
 interface Props {}
-
-interface ISubject extends CombineAdminSubjects {
-  type?: string;
-  title?: string;
-  classNum?: number;
+interface CombineResult {
+  id: number;
+  classes: CombineAdminSubjects;
 }
-
 const AdminSection: FC<Props> = (): ReactElement => {
   const { personalList, teamList, experimentList, loading } = useSelector(
     (state: StoreState) => state.admin,
   );
 
-  const combineSortAllList = (
-    { personal_assignment = [] }: Personal,
-    { team_assignment = [] }: Team,
-    { experiment_assignment = [] }: Experiment,
-  ): ISubject =>
-    [...personal_assignment, ...team_assignment, ...experiment_assignment].sort((a, b) =>
-      a.id > b.id ? 1 : -1,
-    );
+  console.log(1);
 
-  const combineSortAllList2 = (personal: Personal[], team: Team[]) => {
-    let result = [];
-    let add = [];
-
-    for (let i = 0; i < personal.length; i++) {
-      const one = personal[i].personal_assignment;
-      for (let j = 0; j < personal[i].personal_assignment.length; j++) {
-        const two = one[j];
-        console.log(two);
-      }
-    }
+  const pushToResult = (result: CombineResult[], rr: CombineAdminSubject) => {
+    const rrIdx = result.findIndex(a => a.id === rr.id);
+    rrIdx === -1 ? result.push({ id: rr.id, classes: [rr] }) : result[rrIdx].classes.push(rr);
   };
 
-  return (
-    <S.AdminSection>
-      {/* {loading ? (
-        <div>기다리세여!</div>
-      ) : (
-        combineSortAllList(personalList, teamList, experimentList).map(item => {
-          const { id, title } = item;
-          return <Subject key={id} subject={title} onlyPersonal={false} />;
-        })
-      )} */}
-      {console.log(personalList, teamList)}
-      {combineSortAllList2(personalList, teamList)}
+  const sortSubjects = ([...copy]: CombineResult[]) => copy.sort((a, b) => (a.id > b.id ? 1 : -1));
 
-      <Subject onlyPersonal={true} subject={'title'} />
-    </S.AdminSection>
+  const combineSubjects = useCallback(
+    (personal: Personal[], team: Team[], experimentList: Experiment[]) => {
+      const result: CombineResult[] = [];
+      for (let i = 0; i < personal.length; i++) {
+        const p = personal[i].personal_assignment,
+          t = team[i].team_assignment,
+          e = experimentList[i].experiment_assignment;
+        const len = p.length;
+        for (let j = 0; j < len; j++) {
+          pushToResult(result, p[j]);
+          pushToResult(result, t[j]);
+          pushToResult(result, e[j]);
+        }
+      }
+      return sortSubjects(result);
+    },
+    [],
   );
+
+  const renderAllSubjects = () => {
+    const allSubjects = combineSubjects(personalList, teamList, experimentList);
+    return allSubjects.map(subject => {
+      return (
+        <Subject key={subject.id} subject={subject.classes[0].title}>
+          {subject.classes.map((cls, i) => {
+            return <Class key={i} classNum={i + 1} cls={cls} />;
+          })}
+        </Subject>
+      );
+    });
+  };
+
+  return <S.AdminSection>{loading ? <div>Loading~</div> : renderAllSubjects()}</S.AdminSection>;
 };
 
-export default AdminSection;
+export default React.memo(AdminSection);
