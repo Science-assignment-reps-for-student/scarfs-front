d534cfa5f7729b72ee522310cbe0564e5ed242b0
이성진 <chlskatnr1@naver.com>
2020-07-15 23:30:58 +0900

    [CREATE] ExperimentClassSubmit, PersonalClass, PersonalClassInfo
    2020.07.15

---
+++ src/components/Admin/Personal/PersonalClass.tsx
@@ -0,0 +1,41 @@
+import React, { FC, ReactElement, useMemo } from 'react';
+import * as S from '../style';
+import PersonalClassInfo from './PersonalClassInfo';
+import PersonalClassSubmit from './PersonalClassSubmit';
+import { PrEvalCommon } from 'src/modules/reducer/Admin/admin';
+import { PersonalSubject } from 'src/modules/reducer/Admin/adminPersonal';
+
+interface Props {
+  cls: PersonalSubject;
+  classNum: number;
+}
+
+const PersonalClass: FC<Props> = ({ cls, classNum }): ReactElement => {
+  const { created_at, deadline, description, type } = cls;
+
+  const getFullTime = (time: number) => {
+    const c = new Date(time);
+    return `${c.getFullYear()}.${c.getMonth() + 1}.${c.getDate()}`;
+  };
+
+  return (
+    <S.SubjectCls>
+      <S.SubjectClsTitle>{classNum}반</S.SubjectClsTitle>
+      <S.SubjectClsContentWrap>
+        <S.SubjectClsContentHead>
+          <S.SubjectClsContentHeadTitle>{description}</S.SubjectClsContentHeadTitle>
+          <S.SubjectClsContentHeadTime>
+            {getFullTime(created_at)} - {getFullTime(deadline)}
+          </S.SubjectClsContentHeadTime>
+        </S.SubjectClsContentHead>
+        <S.SubjectClsContent>
+          <PersonalClassInfo cls={cls} />
+          <div />
+          <PersonalClassSubmit members={cls.class_submit} />
+        </S.SubjectClsContent>
+      </S.SubjectClsContentWrap>
+    </S.SubjectCls>
+  );
+};
+
+export default PersonalClass;
---
+++ src/components/Admin/Personal/PersonalClassInfo.tsx
@@ -0,0 +1,46 @@
+import React, { FC, ReactElement, useEffect, useRef, useState } from 'react';
+import * as S from '../style';
+import adminDownload from '../../../assets/Admin/adminDownload.svg';
+import adminEdit from '../../../assets/Admin/adminEdit.svg';
+import { PersonalSubject } from 'src/modules/reducer/Admin/adminPersonal';
+
+interface Props {
+  cls: PersonalSubject;
+}
+
+const PersonalClassInfo: FC<Props> = ({ cls }): ReactElement => {
+  const all = cls.class_submit.length;
+  const [o, setO] = useState(0);
+
+  useEffect(() => {
+    cls.class_submit.forEach(a => a.submit === 1 && setO(o + 1));
+  }, [cls]);
+
+  return (
+    <S.SubjectClsContentInfo>
+      <S.InfoSubmitted>
+        <S.InfoSubmittedCommon>
+          <S.InfoSubmittedMembers>
+            제출인원{' '}
+            <span>
+              {o}/{all}
+            </span>
+          </S.InfoSubmittedMembers>
+          <S.AdminProgress max={all} value={o}></S.AdminProgress>
+        </S.InfoSubmittedCommon>
+      </S.InfoSubmitted>
+      <S.SubjectButtonWrap>
+        <S.SubjectButton>
+          <S.SubjectButtonImg src={adminEdit} alt='edit' title='edit' />
+          수정
+        </S.SubjectButton>
+        <S.SubjectButton>
+          <S.SubjectButtonImg src={adminDownload} alt='download' title='download' />
+          다운로드
+        </S.SubjectButton>
+      </S.SubjectButtonWrap>
+    </S.SubjectClsContentInfo>
+  );
+};
+
+export default PersonalClassInfo;
