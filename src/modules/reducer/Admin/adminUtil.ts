7da54dce6ae8806dd70da920a27da6b8ca14c333
이성진 <chlskatnr1@naver.com>
2020-07-15 17:00:01 +0900

    [MODIFY] reducer adminUtil

--- src/modules/reducer/Admin/adminUtil.ts
+++ src/modules/reducer/Admin/adminUtil.ts
@@ -3,55 +3,40 @@ import { Personal } from './adminPersonal';
 import { Team } from './adminTeam';
 import { Experiment } from './adminExperiment';
 
-export const sortPersonalById = (list: Personal) => {
-  const copy = { ...list };
+export const sortPersonalById = (personal: Personal) => {
+  const copy = { ...personal };
   copy.personal_assignment.sort((a, b) => (a.id > b.id ? 1 : -1));
   return copy;
 };
-export const sortTeamById = (list: Team) => {
-  const copy = { ...list };
+export const sortTeamById = (team: Team) => {
+  const copy = { ...team };
   copy.team_assignment.sort((a, b) => (a.id > b.id ? 1 : -1));
   return copy;
 };
-export const sortExperimentById = (list: Experiment) => {
-  const copy = { ...list };
+export const sortExperimentById = (experiment: Experiment) => {
+  const copy = { ...experiment };
   copy.experiment_assignment.sort((a, b) => (a.id > b.id ? 1 : -1));
   return copy;
 };
 
-export const addPropsOfPersonal = (list: Personal) => {
-  const copy = { ...list };
-  copy.personal_assignment.map((item, i) => {
-    return {
-      ...item,
-      title: `[${PERSONAL_STR}] ${item.title}`,
-      type: PERSONAL_STR,
-      classNum: i++,
-    };
+export const addPropsOfPersonal = (personal: Personal) => {
+  const copy = { ...personal };
+  copy.personal_assignment.forEach(cls => {
+    cls.title = `[${PERSONAL_STR}] ${cls.title}`;
   });
   return copy;
 };
-export const addPropsOfTeam = (list: Team) => {
-  const copy = { ...list };
-  copy.team_assignment.map((item, i) => {
-    return {
-      ...item,
-      title: `[${TEAM_STR}] ${item.title}`,
-      type: TEAM_STR,
-      classNum: i++,
-    };
+export const addPropsOfTeam = (team: Team) => {
+  const copy = { ...team };
+  copy.team_assignment.forEach(cls => {
+    cls.title = `[${TEAM_STR}] ${cls.title}`;
   });
   return copy;
 };
-export const addPropsOfExperiment = (list: Experiment) => {
-  const copy = { ...list };
-  copy.experiment_assignment.map((item, i) => {
-    return {
-      ...item,
-      title: `[${EXPERIMENT_STR}] ${item.title}`,
-      type: EXPERIMENT_STR,
-      classNum: i++,
-    };
+export const addPropsOfExperiment = (experiment: Experiment) => {
+  const copy = { ...experiment };
+  copy.experiment_assignment.forEach(cls => {
+    cls.title = `[${EXPERIMENT_STR}] ${cls.title}`;
   });
   return copy;
 };
