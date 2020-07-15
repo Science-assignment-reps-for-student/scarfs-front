d87b946021720c65ceb6b2a5a48847adc715c4f7
이성진 <chlskatnr1@naver.com>
2020-07-15 16:59:12 +0900

    [MODIFY] class

--- src/components/Admin/Class.tsx
+++ src/components/Admin/Class.tsx
@@ -3,21 +3,31 @@ import * as S from './style';
 import ClassInfo from './ClassInfo';
 import ClassListReport from './ClassListReport';
 import ClassListMembers from './ClassListMembers';
+import { CombineAdminSubject } from 'src/modules/reducer/Admin/admin';
 
 interface Props {
-  subject: string;
-  onlyPersonal: boolean;
+  cls: CombineAdminSubject;
   classNum: number;
 }
 
-const Class: FC<Props> = ({ subject, onlyPersonal, classNum }): ReactElement => {
+const Class: FC<Props> = ({ cls, classNum }): ReactElement => {
+  const { created_at, deadline, description } = cls;
+  const onlyPersonal = !cls.title.match('개인');
+
+  const getFullTime = (time: number) => {
+    const c = new Date(time);
+    return `${c.getFullYear()}.${c.getMonth() + 1}.${c.getDate()}`;
+  };
+
   return (
     <S.SubjectCls>
       <S.SubjectClsTitle>{classNum}반</S.SubjectClsTitle>
       <S.SubjectClsContentWrap>
         <S.SubjectClsContentHead>
-          <S.SubjectClsContentHeadTitle>{subject}</S.SubjectClsContentHeadTitle>
-          <S.SubjectClsContentHeadTime>2020.10.23 - 2020.11.01</S.SubjectClsContentHeadTime>
+          <S.SubjectClsContentHeadTitle>{description}</S.SubjectClsContentHeadTitle>
+          <S.SubjectClsContentHeadTime>
+            {getFullTime(created_at)} - {getFullTime(deadline)}
+          </S.SubjectClsContentHeadTime>
         </S.SubjectClsContentHead>
         <S.SubjectClsContent>
           <ClassInfo onlyPersonal={onlyPersonal} />
