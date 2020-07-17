66049373556d611fc59960fe263254863bbc6793
이성진 <chlskatnr1@naver.com>
2020-07-17 19:23:30 +0900

    [MODIFY] AdminLoginContainer
    Connect dummy ccount

--- src/containers/AdminLogin/AdminLoginContainer.tsx
+++ src/containers/AdminLogin/AdminLoginContainer.tsx
@@ -1,13 +1,50 @@
-import React, { FC, ReactElement } from 'react';
-import AdminHeaderContainer from '../AdminHeader/AdminHeaderContainer';
+import React, { FC, ReactElement, useReducer, ChangeEvent } from 'react';
+import { useHistory } from 'react-router-dom';
 import { AdminLogin } from '../../components';
+import AdminHeaderContainer from '../AdminHeader/AdminHeaderContainer';
+import { loginReducer, Login } from '../../modules/reducer/AdminLogin/login';
+import { apiLogin } from '../../lib/api/Admin/admin';
+
 interface Props {}
 
 const AdminLoginContainer: FC<Props> = (): ReactElement => {
+  const history = useHistory();
+  const loginInit: Login = {
+    type: '',
+    ID: '',
+    PW: '',
+  };
+  const [loginState, loginDispatch] = useReducer(loginReducer, loginInit);
+
+  const onChangeLogin = (e: ChangeEvent<HTMLInputElement>) => {
+    loginDispatch({ type: e.target.name, payload: { value: e.target.value } });
+  };
+
+  const onClickLogin = async () => {
+    try {
+      localStorage.setItem('accessToken', 'eya.b.c');
+      localStorage.setItem('refreshToken', 'eya.b.c');
+      history.push('/admin');
+      // const res = await apiLogin(loginState);
+      // const data = res.data;
+      // localStorage.setItem('accessToken', data.access_token);
+      // localStorage.setItem('refreshToken', data.refresh_token);
+    } catch (err) {
+      const code = err.response.status;
+      if (code === 400) {
+        console.log('Bad Request at login');
+      } else if (code === 404) {
+        console.log('Wrong Information at login');
+      } else {
+        console.log('Cannot Found Error 😀');
+      }
+    }
+  };
+
   return (
     <>
       <AdminHeaderContainer />
-      <AdminLogin />  
+      <AdminLogin onChangeLogin={onChangeLogin} onClickLogin={onClickLogin} />
     </>
   );
 };
