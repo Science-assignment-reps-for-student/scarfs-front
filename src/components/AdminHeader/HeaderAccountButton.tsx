638f0ff8d556b7dccfbc2f8e8ed2c8e01b5a32ac
이성진 <chlskatnr1@naver.com>
2020-07-17 19:20:41 +0900

    [MODIFY] HeaderAccountButton
    remove localStorage

--- src/components/AdminHeader/HeaderAccountButton.tsx
+++ src/components/AdminHeader/HeaderAccountButton.tsx
@@ -6,9 +6,14 @@ interface Props {}
 const HeaderAccountButton: FC<Props> = (): ReactElement => {
   const accessToken = localStorage.getItem('accessToken');
 
+  const test = () => {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+  };
+
   return (
     <S.HeaderAccountButtonWrap>
-      <S.HeaderLink to='/admin/login'>
+      <S.HeaderLink to='/admin/login' onClick={test}>
         {accessToken ? '로그아웃' : '로그인'}
         <div className='hovered'></div>
       </S.HeaderLink>
