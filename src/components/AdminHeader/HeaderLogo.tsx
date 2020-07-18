822a94e1656b59f5191c636f0c8279475d006594
이성진 <chlskatnr1@naver.com>
2020-07-18 11:04:18 +0900

    [MODIFY] HeaderLogo, style
    HeaderLogo 메인으로 가기 위한 Link 태그 생성

--- src/components/AdminHeader/HeaderLogo.tsx
+++ src/components/AdminHeader/HeaderLogo.tsx
@@ -5,10 +5,12 @@ interface Props {}
 
 const HeaderLogo: FC<Props> = (): ReactElement => {
   return (
-    <S.HeaderLogoWrap>
-      <S.HeaderLogoScarfs>SCARFS</S.HeaderLogoScarfs>{' '}
-      <S.HeaderLogoConsole>Admin console</S.HeaderLogoConsole>
-    </S.HeaderLogoWrap>
+    <S.HeaderLogoLink to='/' title='메인 페이지 바로가기'>
+      <S.HeaderLogoWrap>
+        <S.HeaderLogoScarfs>SCARFS</S.HeaderLogoScarfs>{' '}
+        <S.HeaderLogoConsole>Admin console</S.HeaderLogoConsole>
+      </S.HeaderLogoWrap>
+    </S.HeaderLogoLink>
   );
 };
 
