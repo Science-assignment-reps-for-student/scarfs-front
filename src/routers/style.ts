fff7332a7d1dbae42a0b07da6783a9fc4cc48fef
_uchanlee <uchanlee.dev@gmail.com>
2020-07-18 16:27:17 +0900

    [FIX] <routers/style.ts> banner width

--- src/routers/style.ts
+++ src/routers/style.ts
@@ -3,7 +3,7 @@ import { BannerImage } from '../assets/Board/Default';
 
 export const Banner = styled.div`
   background-image: url(${BannerImage});
-  width: 100%;
+  min-width: 1340px;
   height: 150px;
   background-size: cover;
 `;
