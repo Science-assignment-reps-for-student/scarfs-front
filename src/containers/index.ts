861db631e7fb1f3b63008b82757d61848055bf7f
_uchanlee <uchanlee.dev@gmail.com>
2020-07-18 16:19:35 +0900

    [ADD] <containers/index.ts> export
    AssignmentGuideBoardContainer

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -3,6 +3,7 @@ import AdminContainer from './Admin/AdminContainer';
 import AdminLoginContainer from './AdminLogin/AdminLoginContainer';
 import AdminHeaderContainer from './AdminHeader/AdminHeaderContainer';
 import { MainContainer } from './Main';
+export { AssignmentGuideBoardContainer } from './Board';
 
 export {
   AdminContainer,
