06880ae9355f37ba7033fcc7c36196d6c39761c1
_uchanlee <uchanlee.dev@gmail.com>
2020-07-18 15:21:40 +0900

    [MODIFY] <CardView> key value

--- src/components/Board/Default/CardView/CardView.tsx
+++ src/components/Board/Default/CardView/CardView.tsx
@@ -3,12 +3,11 @@ import * as S from './style';
 import { AssignmentGuideBoard, ClassBoard, NoticeBoard } from '../TableView';
 
 interface Props {
-  keyName: string;
   boards: Array<AssignmentGuideBoard | ClassBoard | NoticeBoard>;
   CardTemplate?: React.FC<any>;
 }
 
-const CardView: FC<Props> = ({ keyName, boards, CardTemplate }) => {
+const CardView: FC<Props> = ({ boards, CardTemplate }) => {
   return (
     <S.CardViewWrapper>
       <S.Top>
@@ -16,7 +15,7 @@ const CardView: FC<Props> = ({ keyName, boards, CardTemplate }) => {
           const jsx = [];
           for (let i = 0; i < 3; i++) {
             if (typeof boards[i] === 'undefined') break;
-            jsx.push(<CardTemplate key={boards[i][keyName]} board={boards[i]} />);
+            jsx.push(<CardTemplate key={boards[i].id} board={boards[i]} />);
           }
           return jsx;
         })()}
@@ -26,7 +25,7 @@ const CardView: FC<Props> = ({ keyName, boards, CardTemplate }) => {
           const jsx = [];
           for (let i = 3; i < 7; i++) {
             if (typeof boards[i] === 'undefined') break;
-            jsx.push(<CardTemplate key={boards[i][keyName]} board={boards[i]} />);
+            jsx.push(<CardTemplate key={boards[i].id} board={boards[i]} />);
           }
           return jsx;
         })()}
