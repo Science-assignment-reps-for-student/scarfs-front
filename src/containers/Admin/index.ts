1a510b1758a4a7f1520b399757f9d18b06c8851b
이성진 <chlskatnr1@naver.com>
2020-07-20 14:25:40 +0900

    [CREATE] containers/Admin/index.ts
    export로 통일

---
+++ src/containers/Admin/index.ts
@@ -0,0 +1,4 @@
+export { default as AdminContainer } from './AdminContainer';
+export { default as AdminLoginContainer } from './AdminLoginContainer';
+export { default as AdminHeaderContainer } from './AdminHeaderContainer';
+export { default as AdminCreateContainer } from './AdminCreateContainer';
