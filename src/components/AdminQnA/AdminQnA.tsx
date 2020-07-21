071053f04bbff0cac5586d042185d873320de7cf
이성진 <chlskatnr1@naver.com>
2020-07-21 13:30:47 +0900

    [CREATE] AdminQnA

---
+++ src/components/AdminQnA/AdminQnA.tsx
@@ -0,0 +1,10 @@
+import React, { FC, ReactElement } from 'react';
+import * as S from './style';
+
+interface Props {}
+
+const AdminQnA: FC<Props> = (): ReactElement => {
+  return <S.AdminQnAWrap>Test</S.AdminQnAWrap>;
+};
+
+export default AdminQnA;
