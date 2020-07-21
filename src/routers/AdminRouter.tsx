f69fd7c2600643ba254ae01ddb772e74a3e960fc
이성진 <chlskatnr1@naver.com>
2020-07-21 13:31:56 +0900

    [MODIFY] AdminQnA import, export, connect router

--- src/routers/AdminRouter.tsx
+++ src/routers/AdminRouter.tsx
@@ -1,12 +1,19 @@
 import React, { FC, ReactElement } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { AdminContainer, AdminLoginContainer } from '../containers';
+import {
+  AdminContainer,
+  AdminLoginContainer,
+  AdminCreateContainer,
+  AdminQnAContainer,
+} from '../containers';
 import { NotFound } from '../components';
 
 const AdminRouter: FC = (): ReactElement => {
   return (
     <Switch>
       <Route path='/admin/login' component={AdminLoginContainer} />
+      <Route path='/admin/create' component={AdminCreateContainer} />
+      <Route path='/admin/qna' component={AdminQnAContainer} />
       <Route path='/admin/*' component={NotFound} />
       <Route path='/admin' component={AdminContainer} />
     </Switch>
