5bd21b9282abf425a9d1bf5d00f5ab7fb59ba798
이성진 <chlskatnr1@naver.com>
2020-07-22 15:04:59 +0900

    [CREATE] adminSearch.svg export assetes

--- src/assets/Admin/index.ts
+++ src/assets/Admin/index.ts
@@ -1,3 +1,4 @@
 export { default as save } from './adminSave.svg';
 export { default as trash } from './adminTrash.svg';
 export { default as attachment } from './adminAttachment.svg';
+export { default as search } from './adminSearch.svg';
