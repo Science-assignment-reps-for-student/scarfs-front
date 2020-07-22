04887233012302cf56449b6e247a71b302b8785c
이성진 <chlskatnr1@naver.com>
2020-07-22 23:26:49 +0900

    [MODIFY] <components/AdminQnA/AdminQnA.tsx>
    QnA modal setting

--- src/components/AdminQnA/AdminQnA.tsx
+++ src/components/AdminQnA/AdminQnA.tsx
@@ -1,23 +1,29 @@
-import React, { FC, ReactElement, useState, ChangeEvent } from 'react';
+import React, { FC, ReactElement, useState, ChangeEvent, MouseEvent } from 'react';
 import * as S from './style';
 import Header from './Header';
 import AdminTable from './Table';
+import Modal from './Modal';
 
 interface Props {}
 
 const AdminQnA: FC<Props> = (): ReactElement => {
   const [search, setSearch] = useState<string>('');
+  const [modal, setModal] = useState<boolean>(false);
 
   const onChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.currentTarget.value);
+  };
+  const onClickToggleModal = () => {
+    setModal(prev => !prev);
   };
 
   return (
     <S.AdminQnAWrap>
       <S.QnACenter>
-        <Header onChangeSearch={onChangeSearch} />
+        <Header onChangeSearch={onChangeSearch} onClickToggleModal={onClickToggleModal} />
         <AdminTable search={search} />
       </S.QnACenter>
+      {modal && <Modal onClickToggleModal={onClickToggleModal} />}
     </S.AdminQnAWrap>
   );
 };
