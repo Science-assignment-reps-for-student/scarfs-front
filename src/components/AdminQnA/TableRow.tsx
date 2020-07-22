e2928882d1cf8e2d7c509726c832251a7ec7d3ad
이성진 <chlskatnr1@naver.com>
2020-07-22 15:04:24 +0900

    [CREATE] Admin TableRow

---
+++ src/components/AdminQnA/TableRow.tsx
@@ -0,0 +1,29 @@
+import React, { FC, ReactElement } from 'react';
+import * as S from './style';
+
+interface Props {
+  student_number: string;
+  name: string;
+  last_message: string;
+  current_date: string;
+  read: boolean;
+}
+
+const TableRow: FC<Props> = ({
+  current_date,
+  last_message,
+  name,
+  read,
+  student_number,
+}): ReactElement => {
+  return (
+    <S.QnARow>
+      <S.QnARowItem>{student_number}</S.QnARowItem>
+      <S.QnARowItem>{name}</S.QnARowItem>
+      <S.QnARowItem className={!read && 'unRead'}>{last_message}</S.QnARowItem>
+      <S.QnARowItem>{current_date}</S.QnARowItem>
+    </S.QnARow>
+  );
+};
+
+export default TableRow;
