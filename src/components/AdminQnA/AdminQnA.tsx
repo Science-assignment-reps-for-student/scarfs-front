2cad35e7fab172c863080ee8b6a291f6604bbef3
이성진 <chlskatnr1@naver.com>
2020-07-22 15:05:16 +0900

    [MODIFY] AmdinQnA 컴포넌트화

--- src/components/AdminQnA/AdminQnA.tsx
+++ src/components/AdminQnA/AdminQnA.tsx
@@ -1,10 +1,25 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useState, ChangeEvent } from 'react';
 import * as S from './style';
+import Header from './Header';
+import AdminTable from './Table';
 
 interface Props {}
 
 const AdminQnA: FC<Props> = (): ReactElement => {
-  return <S.AdminQnAWrap>Test</S.AdminQnAWrap>;
+  const [search, setSearch] = useState<string>('');
+
+  const onChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.currentTarget.value);
+  };
+
+  return (
+    <S.AdminQnAWrap>
+      <S.QnACenter>
+        <Header onChangeSearch={onChangeSearch} />
+        <AdminTable search={search} />
+      </S.QnACenter>
+    </S.AdminQnAWrap>
+  );
 };
 
 export default AdminQnA;
