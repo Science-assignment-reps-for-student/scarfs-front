96d7068c5d5a7d9c8f20b1bed047169b21dcdb8c
이성진 <chlskatnr1@naver.com>
2020-07-23 10:51:26 +0900

    [CREATE] <assets/Admin/adminSend.svg>
    send svg export

--- src/assets/Admin/index.ts
+++ src/assets/Admin/index.ts
@@ -3,3 +3,4 @@ export { default as trash } from './adminTrash.svg';
 export { default as attachment } from './adminAttachment.svg';
 export { default as search } from './adminSearch.svg';
 export { default as close } from './adminCloseWhite.svg';
+export { default as send } from './adminSend.svg';
