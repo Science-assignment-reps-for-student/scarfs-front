0a59e65db3c210e070bc53ee1d342abee5cd3aca
이성진 <chlskatnr1@naver.com>
2020-07-27 21:56:38 +0900

    [CREATE] <components/AlertModal/*>
    AlertModal 생성

---
+++ src/components/AlertModal/AlertModal.tsx
@@ -0,0 +1,9 @@
+import React, { FC, ReactElement } from 'react';
+
+interface Props {}
+
+const AlertModal: FC<Props> = (): ReactElement => {
+  return <div></div>;
+};
+
+export default AlertModal;
