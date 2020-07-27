99186ce3e971e4b5ee1203126a9a06c6a3700ef5
이성진 <chlskatnr1@naver.com>
2020-07-27 22:20:18 +0900

    Auto stash before merge of "feature/AlertModal" and "feature/Admin_Api_Connect"

--- src/components/AlertModal/AlertModal.tsx
+++ src/components/AlertModal/AlertModal.tsx
@@ -1,9 +1,37 @@
 import React, { FC, ReactElement } from 'react';
+import { warning, notification } from '../../assets/Admin';
+import * as S from './style';
 
-interface Props {}
+type AcceptTypes = 'warn' | 'notify';
 
-const AlertModal: FC<Props> = (): ReactElement => {
-  return <div></div>;
+interface Props {
+  type: AcceptTypes;
+}
+
+const AlertModal: FC<Props> = ({ type }): ReactElement => {
+  // Warning, Notification
+
+  enum Images {
+    warn = warning,
+    notify = notification,
+  }
+
+  Images['warn'];
+
+  return (
+    <div>
+      <div className='back'></div>
+      <p>
+        <img src={notification} alt='notification' title='notification' />
+        <span>알림</span>
+      </p>
+      <p>모든 빈칸을 채워주세요.</p>
+      <div>
+        <button>확인</button>
+        <button>취소</button>
+      </div>
+    </div>
+  );
 };
 
 export default AlertModal;
