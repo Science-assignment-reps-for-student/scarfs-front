27e5f24bb7a7709c2d612afcd132e2406d55b1ed
이성진 <chlskatnr1@naver.com>
2020-07-28 22:11:16 +0900

    [MODIFY] containers/Admin/[Container, Create, QnA]
    localStorage accessToken 확인 함수 추가

--- src/containers/Admin/AdminCreateContainer.tsx
+++ src/containers/Admin/AdminCreateContainer.tsx
@@ -1,9 +1,18 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import AdminCreate from '../../components/AdminCreate/AdminCreate';
 import AdminHeaderContainer from './AdminHeaderContainer';
 interface Props {}
 
 const AdminCreateContainer: FC<Props> = (): ReactElement => {
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!localStorage.getItem('accessToken')) {
+      history.push('/admin/login');
+    }
+  }, []);
+
   return (
     <>
       <AdminHeaderContainer />
