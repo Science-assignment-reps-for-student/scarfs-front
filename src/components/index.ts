a69e2bbbad12a28e86cf8ac4bd4f818f9e9ddc0c
이성진 <chlskatnr1@naver.com>
2020-07-28 23:21:53 +0900

    [MODIFY] <AdminCreateContainer>
    AdminCreate 경로 변경

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export { default as Admin } from './Admin/AdminMain/Admin';
 export { default as AdminHeader } from './Admin/AdminHeader/AdminHeader';
 export { default as AdminLogin } from './Admin/AdminLogin/AdminLogin';
 export { default as AdminQnA } from './Admin/AdminQnA/AdminQnA';
+export { default as AdminCreate } from './Admin/AdminCreate/AdminCreate';
 export { NotFound } from './NotFound';
 export { Wave, Header } from './Default';
 export { default as AlertModal } from './AlertModal/AlertModal';
