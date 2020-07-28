bca8a949ace669870796d1b9c15ad693a41a6f3c
이성진 <chlskatnr1@naver.com>
2020-07-28 22:12:14 +0900

    [MODIFY] <components/AdminHeader/HeaderNav>
    Link 가기 전 accessToken 확인하는 함수 추가

--- src/components/AdminHeader/HeaderNav.tsx
+++ src/components/AdminHeader/HeaderNav.tsx
@@ -1,30 +1,52 @@
-import React, { FC, ReactElement } from 'react';
-import * as S from './style';
+import React, { FC, ReactElement, MouseEvent } from 'react';
 import { useLocation } from 'react-router-dom';
+import * as S from './style';
 
 interface Props {}
 
 const HeaderNav: FC<Props> = (): ReactElement => {
   const location = useLocation();
 
+  const stopEvt = (e: MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+  const onClickCheckAccessToken = (e: MouseEvent<HTMLAnchorElement>) => {
+    if (!localStorage.getItem('accessToken')) {
+      stopEvt(e);
+    }
+  };
+
   return (
     <S.HeaderNavWrap>
       <S.HeaderNavItem>
-        <S.HeaderLink to='/admin' className={location.pathname === '/admin' && 'visit'}>
-          제출현황<div className='hovered'></div>
+        <S.HeaderLink
+          to='/admin'
+          onClick={onClickCheckAccessToken}
+          className={location.pathname === '/admin' && 'visit'}
+        >
+          제출현황
+          <div className='hovered' />
         </S.HeaderLink>
       </S.HeaderNavItem>
       <S.HeaderNavItem>
         <S.HeaderLink
           to='/admin/create'
+          onClick={onClickCheckAccessToken}
           className={location.pathname === '/admin/create' && 'visit'}
         >
-          과제생성<div className='hovered'></div>
+          과제생성
+          <div className='hovered' />
         </S.HeaderLink>
       </S.HeaderNavItem>
       <S.HeaderNavItem>
-        <S.HeaderLink to='/admin/qna' className={location.pathname === '/admin/qna' && 'visit'}>
-          QnA<div className='hovered'></div>
+        <S.HeaderLink
+          to='/admin/qna'
+          onClick={onClickCheckAccessToken}
+          className={location.pathname === '/admin/qna' && 'visit'}
+        >
+          QnA
+          <div className='hovered' />
         </S.HeaderLink>
       </S.HeaderNavItem>
     </S.HeaderNavWrap>
