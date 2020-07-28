237d8e9c7d0ef8d44473bad2da5b3ba2d8f6c93c
_uchanlee <uchanlee.dev@gmail.com>
2020-07-28 18:31:04 +0900

    [CREATE] <DetailPost/NoticeDetailPost/NoticeDetailPostContainer.tsx>
    container

---
+++ src/containers/Board/DetailPost/NoticeDetailPost/NoticeDetailPostContainer.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { NoticeDetailPost } from '../../../../components';
+
+const NoticeDetailPostContainer: FC<{}> = () => {
+  return <NoticeDetailPost />;
+};
+
+export default NoticeDetailPostContainer;
