12c952be9c2844828916de6c7813b1cce7a00785
이성진 <chlskatnr1@naver.com>
2020-07-29 21:39:41 +0900

    [CREATE] style

---
+++ src/components/Admin/AdminSignUp/style.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const AdminSignUpWrap = styled.div`
+  border: 1px solid red;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: blue;
+`;
+
+export const SignUpFormWrap = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, calc(-50% - 0.5px));
+`;
+
+export const SignUpLabel = styled.label``;
+
+export const SignUpInput = styled.input``;
