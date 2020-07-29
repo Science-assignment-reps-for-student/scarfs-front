57272ee491921ed4d6e10868e18934ed6d3d67a6
ohjunsang <pandati@dsm.hs.kr>
2020-07-29 12:56:06 +0900

    [FIX] redux에서 data 받아오게 수정

--- src/components/Main/Login/UserInfo/UserInfo.tsx
+++ src/components/Main/Login/UserInfo/UserInfo.tsx
@@ -1,15 +1,26 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import * as S from '../../style';
 import UserInfoTask from './UserInfoTask';
 import UserInfoButton from './UserInfoButton';
+import { getUserInfo, MainState } from '../../../../modules/reducer/Main';
+import { stateChange, getStateCallback } from '../../../../lib/function';
 
-const UserInfo: FC = ({}) => {
+const UserInfo: FC = () => {
+  const { userInfo } = useSelector(getStateCallback<MainState>('Main'));
+  const getUserInfoChange = stateChange(getUserInfo);
+  useEffect(() => {
+    getUserInfoChange();
+  }, []);
   return (
     <S.UserMain>
       <div>
         <S.UserInfo>
-          <S.UserInfoName>1학년 2반 준상오</S.UserInfoName>
-          <UserInfoTask />
+          <S.UserInfoName>{userInfo.name}</S.UserInfoName>
+          <UserInfoTask
+            unCompleteTaskCount={userInfo.remainingAssignment}
+            completeTaskCount={userInfo.completionAssignment}
+          />
         </S.UserInfo>
         <UserInfoButton />
       </div>
