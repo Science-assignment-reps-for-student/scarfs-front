4d0d9670764f10db100d1559f4b9e5da5130a461
이성진 <chlskatnr1@naver.com>
2020-07-29 21:39:48 +0900

    [CREATE] Container

---
+++ src/containers/Admin/AdminSignUpContainer.tsx
@@ -0,0 +1,10 @@
+import React, { FC, ReactElement } from 'react';
+import AdminSignUp from '../../components/Admin/AdminSignUp/AdminSignUp';
+
+interface Props {}
+
+const AdminSignUpContainer: FC<Props> = (): ReactElement => {
+  return <AdminSignUp />;
+};
+
+export default AdminSignUpContainer;
