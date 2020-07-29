d8926d1bbe62d0b9ee0fcaffc140fc17a5995d5f
ohjunsang <pandati@dsm.hs.kr>
2020-07-29 13:07:22 +0900

    [FIX] main thunk fix

--- src/modules/thunk/Main/index.ts
+++ src/modules/thunk/Main/index.ts
@@ -1,24 +1,49 @@
-import { GET_ASSIGNMENT_FAILURE, ASSIGNMENT, GET_BOARD_FAILURE, BOARD } from '../../reducer/Main';
-import { getAssignment, getBoard } from '../../../lib/api/Assignment/Assignment';
+import {
+  GET_ASSIGNMENT_FAILURE,
+  GET_BOARD_FAILURE,
+  GET_BOARD_SUCCESS,
+  GET_ASSIGNMENT_SUCCESS,
+  GET_USER_INFO_SUCCESS,
+  GET_USER_INFO_FAILURE,
+  LOADING,
+} from '../../reducer/Main';
+import { getAssignment, getBoard, getUserInfo } from '../../../lib/api/Assignment/Assignment';
 
 export const getBoardThunk = () => {
   return () => async dispatch => {
     try {
+      dispatch({ type: LOADING, payload: true });
       const payload = await getBoard();
-      dispatch({ type: BOARD, payload });
+      dispatch({ type: GET_BOARD_SUCCESS, payload });
     } catch (err) {
       dispatch({ type: GET_BOARD_FAILURE, payload: err });
     }
+    dispatch({ type: LOADING, payload: false });
   };
 };
 
 export const getAssignmentThunk = () => {
   return () => async dispatch => {
     try {
+      dispatch({ type: LOADING, payload: true });
       const payload = await getAssignment();
-      dispatch({ type: ASSIGNMENT, payload });
+      dispatch({ type: GET_ASSIGNMENT_SUCCESS, payload });
     } catch (err) {
       dispatch({ type: GET_ASSIGNMENT_FAILURE, payload: err });
     }
+    dispatch({ type: LOADING, payload: false });
+  };
+};
+
+export const getUserInfoThunk = () => {
+  return () => async dispatch => {
+    try {
+      dispatch({ type: LOADING, payload: true });
+      const payload = await getUserInfo;
+      dispatch({ type: GET_USER_INFO_SUCCESS, payload });
+    } catch (err) {
+      dispatch({ type: GET_USER_INFO_FAILURE, payload: err });
+    }
+    dispatch({ type: LOADING, payload: false });
   };
 };
