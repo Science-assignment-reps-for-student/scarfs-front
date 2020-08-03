1f0c55025f4a228074f0b83add79bb8b9608f1d4
_uchanlee <uchanlee.dev@gmail.com>
2020-08-03 15:05:34 +0900

    [ADD] <components/~/ClassBoard> write button
    write class post button

---
+++ src/components/Board/ClassBoard/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Footer = styled.footer``;
+
+export const Button = styled.button`
+  cursor: pointer;
+  width: 131px;
+  height: 49px;
+  color: #ffffff;
+  background-color: #1d1d1d;
+  border: none;
+  outline: none;
+  box-shadow: 2px 2px rgba(0, 0, 0, 0.1);
+  float: right;
+  border-radius: 0;
+  margin-top: -28px;
+`;
