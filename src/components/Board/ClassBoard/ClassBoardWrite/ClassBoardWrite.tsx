2c7c7594946d2383a32419fe7c8d397abb339161
_uchanlee <uchanlee.dev@gmail.com>
2020-08-07 08:57:55 +0900

    [ADD] <components/~/ClassBoardWrite.tsx>
    edit mode

--- src/components/Board/ClassBoard/ClassBoardWrite/ClassBoardWrite.tsx
+++ src/components/Board/ClassBoard/ClassBoardWrite/ClassBoardWrite.tsx
@@ -1,12 +1,23 @@
 import React, { FC, createElement, useRef, useState } from 'react';
 import * as S from './style';
 import { Header, WriteMain } from './';
+import queryString from 'query-string';
+import { Redirect } from 'react-router-dom';
+
+const data = {
+  title: '제목이얌',
+  content: '이건 내용이야',
+};
 
 const ClassBoardWrite: FC = () => {
+  const { id } = queryString.parse(location.search);
+  if (typeof id !== 'undefined' && isNaN(Number(id))) {
+    return <Redirect to='error' />;
+  }
   return (
     <>
-      <Header title='게시판 작성' />
-      <WriteMain />
+      <Header title={id ? '게시판 수정' : '게시판 작성'} />
+      <WriteMain data={id ? data : { title: '', content: '' }} />
     </>
   );
 };
