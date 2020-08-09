821e6833f8aebc1599acc4333fdf04c12c68fc9d
ohjunsang <pandati@dsm.hs.kr>
2020-08-09 20:18:23 +0900

    [FIX] button interface 수정으로 인한 변경

--- src/components/Default/Header/components/Header.tsx
+++ src/components/Default/Header/components/Header.tsx
@@ -7,8 +7,14 @@ import HeaderSearch from './HeaderSearch';
 import HeaderUserButton from './HeaderUserButton';
 import { Link } from 'react-router-dom';
 
-const Header: FC = () => {
-  const modalChange = stateChange(setModal);
+interface Props {
+  isLogin: boolean;
+  modalChange: (value: string) => void;
+  logoutHandler: (value: boolean) => void;
+  userName?: string;
+}
+
+const Header: FC<Props> = ({ logoutHandler, modalChange, isLogin, userName }) => {
   return (
     <S.Header>
       <div className='header'>
@@ -17,20 +23,38 @@ const Header: FC = () => {
             <S.HeaderTitle>SCARFS</S.HeaderTitle>
           </Link>
           <HeaderSearch />
-          <HeaderButton link='/'>메인</HeaderButton>
-          <HeaderButton link='/board/notice'>공지사항</HeaderButton>
-          <HeaderButton link='/board/assignment-guide'>과제</HeaderButton>
-          <HeaderButton link='/board/class'>게시판</HeaderButton>
+          <HeaderButton isLogin={isLogin} link='/'>
+            메인
+          </HeaderButton>
+          <HeaderButton isLogin={isLogin} link='/board/notice'>
+            공지사항
+          </HeaderButton>
+          <HeaderButton isLogin={isLogin} link='/board/assignment-guide'>
+            과제
+          </HeaderButton>
+          <HeaderButton isLogin={isLogin} link='/board/class'>
+            게시판
+          </HeaderButton>
         </div>
-        <S.HeaderUserButtonWrapper>
-          <HeaderUserButton onClick={modalChange} value='SignIn'>
-            로그인
-          </HeaderUserButton>
-          <div className='bar' />
-          <HeaderUserButton onClick={modalChange} value='SignUpInfo'>
-            회원가입
-          </HeaderUserButton>
-        </S.HeaderUserButtonWrapper>
+        {isLogin ? (
+          <S.HeaderUserButtonWrapper>
+            <p>{userName}님 안녕하세요.</p>
+            <div className='bar' />
+            <HeaderUserButton onClick={logoutHandler} value=''>
+              로그아웃
+            </HeaderUserButton>
+          </S.HeaderUserButtonWrapper>
+        ) : (
+          <S.HeaderUserButtonWrapper>
+            <HeaderUserButton onClick={modalChange} value='SignIn'>
+              로그인
+            </HeaderUserButton>
+            <div className='bar' />
+            <HeaderUserButton onClick={modalChange} value='SignUpInfo'>
+              회원가입
+            </HeaderUserButton>
+          </S.HeaderUserButtonWrapper>
+        )}
       </div>
     </S.Header>
   );
