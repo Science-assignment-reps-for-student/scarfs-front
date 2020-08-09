b0d166aaed054eee797efa67e64af9b3f2148f25
ohjunsang <pandati@dsm.hs.kr>
2020-08-09 22:46:42 +0900

    [FIX] 로그인 상태 아니면 안뜨게 수정

--- src/containers/Chatting/ChattingContainer.tsx
+++ src/containers/Chatting/ChattingContainer.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import HeaderState from 'src/modules/reducer/Header';
 import Chatting, { ChattingOpenButton } from '../../components/Chatting';
 import { getStateCallback } from '../../lib/function';
 import { ChattingState, setIsAble } from '../../modules/reducer/Chatting';
@@ -7,10 +8,19 @@ import { ChattingState, setIsAble } from '../../modules/reducer/Chatting';
 const ChattingContainer: FC = () => {
   const dispatch = useDispatch();
   const { isAble } = useSelector(getStateCallback<ChattingState>('Chatting'));
+  const { isLogin } = useSelector(getStateCallback<HeaderState>('Header'));
   const isAbleChange = useCallback((isAble: boolean) => {
     dispatch(setIsAble(isAble));
   }, []);
-  return isAble ? <Chatting /> : <ChattingOpenButton isAbleChange={isAbleChange} />;
+  return isLogin ? (
+    isAble ? (
+      <Chatting />
+    ) : (
+      <ChattingOpenButton isAbleChange={isAbleChange} />
+    )
+  ) : (
+    <></>
+  );
 };
 
 export default ChattingContainer;
