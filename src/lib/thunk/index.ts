759d5e8fbf1a5f4adc2f969f1114d9637dc6e913
_uchanlee <uchanlee.dev@gmail.com>
2020-08-12 08:37:19 +0900

    [CREATE] <lib/thunk/index.ts> createRequestThunk

---
+++ src/lib/thunk/index.ts
@@ -0,0 +1,29 @@
+import { startLoading, finishLoading } from '../../modules/reducer/Loading';
+import { ErrorType } from 'src/modules/reducer/Modal';
+
+const createRequestThunk = (type, request) => {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+  return params => async dispatch => {
+    dispatch({ type });
+    dispatch(startLoading(type));
+    try {
+      const response = await request(params);
+      dispatch({
+        type: SUCCESS,
+        payload: response.data,
+      });
+      dispatch(finishLoading(type));
+    } catch (e) {
+      const error: ErrorType = e;
+      dispatch({
+        type: FAILURE,
+        payload: error,
+      });
+      dispatch(startLoading(type));
+      throw e;
+    }
+  };
+};
+
+export { createRequestThunk };
