9fe0b17ad3ede87089889e34d7ebe590450e6aef
_uchanlee <uchanlee.dev@gmail.com>
2020-08-12 17:21:16 +0900

    [ADD] <components/~/ClassBoard.tsx>
    skeleton when loading and connect api

--- src/components/Board/ClassBoard/ClassBoard.tsx
+++ src/components/Board/ClassBoard/ClassBoard.tsx
@@ -1,9 +1,11 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect, useMemo } from 'react';
 import { CardView } from '../Default';
 import { BoardHeader, TableView, PaginationBar } from '../Default';
 import { ClassCard, ClassTableItem } from './';
 import * as S from './style';
 import { useHistory } from 'react-router-dom';
+import { SBone } from '../../../components/Admin/AdminMain/style';
+import { ClassBoard } from '../../../lib/api/ClassBoard';
 
 const dummyBoards = [
   {
@@ -83,18 +85,46 @@ const boards = dummyBoards.map(board => ({
   id: board.boardId,
 }));
 
-const ClassBoard: FC = () => {
+interface Props {
+  isLoading: boolean;
+  classBoard: ClassBoard;
+  getBoard: (data: { size: number; page: number }) => void;
+}
+
+const ClassBoard: FC<Props> = ({ isLoading, classBoard, getBoard }) => {
+  const boards = useMemo(
+    () =>
+      classBoard.boardResponses.map(board => ({
+        ...board,
+        id: board.boardId,
+      })),
+    [classBoard],
+  );
   const history = useHistory();
+  const [page, setPage] = useState(1);
   const [isTableView, setIsTableView] = useState(true);
+  useEffect(() => {
+    const ONE_PAGE_BOARD_SIZE = 7;
+    getBoard({
+      size: ONE_PAGE_BOARD_SIZE,
+      page: page,
+    });
+  }, [page]);
   return (
     <>
-      <BoardHeader
-        title='2반 게시판'
-        searchTitle=''
-        isTableView={isTableView}
-        setIsTableView={setIsTableView}
-      />
-      {isTableView ? (
+      {isLoading ? (
+        <SBone width='1280px' height='45px' />
+      ) : (
+        <BoardHeader
+          title='2반 게시판'
+          searchTitle=''
+          isTableView={isTableView}
+          setIsTableView={setIsTableView}
+        />
+      )}
+      {isLoading ? (
+        <SBone width='1280px' height='362px' />
+      ) : isTableView ? (
         <TableView
           columnNames={['유형', '제목', '작성자', '등록일', '조회수']}
           boards={boards}
@@ -103,10 +133,14 @@ const ClassBoard: FC = () => {
       ) : (
         <CardView boards={boards} CardTemplate={ClassCard} />
       )}
-      <S.Footer>
-        <PaginationBar />
-        <S.Button onClick={() => history.push('/board/class/write')}>게시물작성</S.Button>
-      </S.Footer>
+      {isLoading ? (
+        <SBone width='1280px' height='50px' />
+      ) : (
+        <S.Footer>
+          <PaginationBar page={page} setPage={setPage} lastPage={classBoard.totalPages} />
+          <S.Button onClick={() => history.push('/board/class/write')}>게시물작성</S.Button>
+        </S.Footer>
+      )}
     </>
   );
 };
