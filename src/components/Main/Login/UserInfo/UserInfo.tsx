bfc7fc7bf81b086a902fffaad126e8ed5d55b29f
ohjunsang <pandati@dsm.hs.kr>
2020-08-12 22:27:38 +0900

    [FIX] 의존성을 낮춤

--- src/components/Main/Login/UserInfo/UserInfo.tsx
+++ src/components/Main/Login/UserInfo/UserInfo.tsx
@@ -1,51 +1,33 @@
-import React, { FC, useEffect, useCallback } from 'react';
-import { useSelector } from 'react-redux';
+import React, { FC } from 'react';
 import * as S from '../../style';
 import UserInfoTask from './UserInfoTask';
 import UserInfoButton from './UserInfoButton';
-import { getUserInfo, MainState } from '../../../../modules/reducer/Main';
-import { stateChange, getStateCallback, isNetworkError } from '../../../../lib/function';
-import { HeaderState, sendRefreshToken } from '../../../../modules/reducer/Header';
+import { UserInfoType } from 'lib/api/Assignment/Assignment';
+import ErrorUserInfo from './ErrorUserInfo';
 
-const UserInfo: FC = () => {
-  const { userInfo, error } = useSelector(getStateCallback<MainState>('Main'));
-  const { loading, refreshToken } = useSelector(getStateCallback<HeaderState>('Header'));
-  const refreshTokenChange = stateChange(sendRefreshToken);
-  const getUserInfoChange = stateChange(getUserInfo);
-  const serverErrorHandler = useCallback((status: number) => {
-    switch (status) {
-      case 401: {
-        const params = {
-          serverType: {
-            refreshToken,
-          },
-          loading,
-          callback: getUserInfoChange,
-        };
-        refreshTokenChange(params);
-      }
-    }
-  }, []);
-  useEffect(() => {
-    getUserInfoChange();
-  }, []);
-  useEffect(() => {
-    if (!error) return;
-    if (isNetworkError(error)) return;
-    const statusCode = error.response.status;
-    serverErrorHandler(statusCode);
-  }, [error]);
+interface Props {
+  userInfo: UserInfoType | null;
+  isLoading: boolean;
+}
+
+const UserInfo: FC<Props> = ({ userInfo, isLoading }) => {
   return (
     <S.UserMain>
       <div>
-        <S.UserInfo>
-          <S.UserInfoName>{userInfo.name}</S.UserInfoName>
-          <UserInfoTask
-            unCompleteTaskCount={userInfo.remainingAssignment}
-            completeTaskCount={userInfo.completionAssignment}
-          />
-        </S.UserInfo>
-        <UserInfoButton />
+        {isLoading || !userInfo ? (
+          <ErrorUserInfo />
+        ) : (
+          <>
+            <S.UserInfo>
+              <S.UserInfoName>{userInfo.name}</S.UserInfoName>
+              <UserInfoTask
+                unCompleteTaskCount={userInfo.remainingAssignment}
+                completeTaskCount={userInfo.completionAssignment}
+              />
+            </S.UserInfo>
+            <UserInfoButton />
+          </>
+        )}
       </div>
     </S.UserMain>
   );
