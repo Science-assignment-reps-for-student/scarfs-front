d21612334124acbca0d491399eb79a10ed749a70
_uchanlee <uchanlee.dev@gmail.com>
2020-08-12 17:19:08 +0900

    [UPDATE] <components/~/PaginationBar.tsx>
    pagination logic

--- src/components/Board/Default/PaginationBar/PaginationBar.tsx
+++ src/components/Board/Default/PaginationBar/PaginationBar.tsx
@@ -1,18 +1,49 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo, useCallback } from 'react';
 import * as S from './style';
 
-const PaginationBar: FC = () => {
+interface Props {
+  page?: number;
+  setPage?: React.Dispatch<React.SetStateAction<number>>;
+  lastPage?: number;
+}
+
+const isCanGoPrevPage = changePage => (changePage >= 1 ? true : false);
+const isCanGoNextPage = (changePage, lastPage) => (changePage <= lastPage ? true : false);
+const PaginationBar: FC<Props> = ({ page, setPage, lastPage }) => {
+  const startPage = useMemo(() => Math.floor((page - 1) / 5) * 5 + 1, [page]);
+  const prevChangePage = useMemo(() => startPage - 1, [startPage]);
+  const nextChangePage = useMemo(() => startPage + 5, [startPage]);
+  const goPrevPage = useCallback(() => {
+    if (isCanGoPrevPage(prevChangePage)) setPage(prevChangePage);
+  }, [prevChangePage]);
+  const goNextPage = useCallback(() => {
+    if (isCanGoNextPage(nextChangePage, lastPage)) setPage(nextChangePage);
+  }, [nextChangePage, lastPage]);
+  const getPageButtons = useCallback(() => {
+    const jsx = [];
+    for (let i = startPage; i < startPage + 5; i++) {
+      if (i > lastPage) break;
+      jsx.push(
+        <S.PageButton key={i} className={page === i && 'active'} onClick={() => setPage(i)}>
+          {i}
+        </S.PageButton>,
+      );
+    }
+    return jsx;
+  }, [startPage]);
   return (
     <S.PaginationBarWrapper>
-      <S.PageControllButton>◀</S.PageControllButton>
-      <S.PageBlock>
-        <S.PageButton className='active'>1</S.PageButton>
-        <S.PageButton>2</S.PageButton>
-        <S.PageButton>3</S.PageButton>
-        <S.PageButton>4</S.PageButton>
-        <S.PageButton>5</S.PageButton>
-      </S.PageBlock>
-      <S.PageControllButton>▶</S.PageControllButton>
+      <nav>
+        {isCanGoPrevPage(prevChangePage) && (
+          <S.PageControllButton onClick={goPrevPage}>◀</S.PageControllButton>
+        )}
+      </nav>
+      <S.PageBlock>{getPageButtons()}</S.PageBlock>
+      <nav>
+        {isCanGoNextPage(nextChangePage, lastPage) && (
+          <S.PageControllButton onClick={goNextPage}>▶</S.PageControllButton>
+        )}
+      </nav>
     </S.PaginationBarWrapper>
   );
 };
