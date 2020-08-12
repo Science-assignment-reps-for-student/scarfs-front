0e66a6e163be32c1127a893a277784c2dc2d0c1a
_uchanlee <uchanlee.dev@gmail.com>
2020-08-12 09:47:40 +0900

    [MODIFY] <lib/thunk/index.ts> delete function
    타입 추론 불가

--- src/lib/thunk/index.ts
+++ src/lib/thunk/index.ts
@@ -24,9 +24,3 @@ export const createRequestThunk = (type, request) => {
     }
   };
 };
-
-export const createRequestActionTypes = (type: string) => {
-  const SUCCESS = `${type}_SUCCESS`;
-  const FAILURE = `${type}_FAILURE`;
-  return [type, SUCCESS, FAILURE];
-};
