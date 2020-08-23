b054a05da4d2ea9c8992414fe3388e53f8407aad
ohjunsang <pandati@dsm.hs.kr>
2020-08-23 14:49:35 +0900

    [FIX] naming

--- src/containers/Main/MainContainer.tsx
+++ src/containers/Main/MainContainer.tsx
@@ -3,23 +3,27 @@ import { useSelector } from 'react-redux';
 import {
   GET_ASSIGNMENT,
   GET_USER_INFO,
-  GET_BOARD_CALL,
+  GET_BOARD_MAIN,
   MainState,
-  getAssignment,
-  getBoard,
-  getUserInfo,
 } from '../../modules/reducer/Main';
 import { LoginedMain, LogOutedMain } from '../../components/Main';
 import { getStateCallback, isNetworkError, stateChange } from '../../lib/function/index';
 import { HeaderState, sendRefreshToken, setIsLogin } from '../../modules/reducer/Header';
 import { LoadingState } from 'src/modules/reducer/Loading';
+import {
+  getAssignmentThunk,
+  getBoardThunk,
+  getUserInfoThunk,
+  logout,
+} from '../../modules/thunk/Main';
 
 const Main: FC = () => {
-  const getAssignmentChange = stateChange<{ size: number; page: number }>(getAssignment);
-  const getBoardChange = stateChange<{ size: number; page: number }>(getBoard);
+  const getAssignmentChange = stateChange<{ size: number; page: number }>(getAssignmentThunk);
+  const getBoardChange = stateChange<{ size: number; page: number }>(getBoardThunk);
   const isLoginChange = stateChange<boolean>(setIsLogin);
   const refreshTokenChange = stateChange(sendRefreshToken);
-  const getUserInfoChange = stateChange(getUserInfo);
+  const getUserInfoChange = stateChange(getUserInfoThunk);
+  const logoutChange = stateChange(logout);
   const { isLogin, refreshToken } = useSelector(getStateCallback<HeaderState>('Header'));
   const { assignmentPreview, boardPreview, error, userInfo } = useSelector(
     getStateCallback<MainState>('Main'),
@@ -56,8 +60,9 @@ const Main: FC = () => {
       boardTask={boardPreview}
       userInfo={userInfo}
       isAssignmentLoading={LoadingState[GET_ASSIGNMENT]}
-      isBoardLoading={LoadingState[GET_BOARD_CALL]}
+      isBoardLoading={LoadingState[GET_BOARD_MAIN]}
       isUserInfoLoading={LoadingState[GET_USER_INFO]}
+      logout={logoutChange}
     />
   ) : (
     <LogOutedMain />
