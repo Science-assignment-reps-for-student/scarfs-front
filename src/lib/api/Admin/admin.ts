c52ef22cb2d07db5a4f680aeacf93390cac3fa1d
이성진 <chlskatnr1@naver.com>
2020-08-25 20:34:06 +0900

    [MODIFY] <api/Admin> 어드민 api uri 변경

--- src/lib/api/Admin/admin.ts
+++ src/lib/api/Admin/admin.ts
@@ -6,13 +6,13 @@ import { Personal } from '../../../modules/reducer/Admin/adminPersonal';
 import { Experiment } from '../../../modules/reducer/Admin/adminExperiment';
 
 export const getAssignmentPersonal = (classNum: number) => {
-  return getApiDefault().get<Personal>(`/admin/personal-assignment?class=${classNum}`);
+  return getApiDefault().get<Personal>(`/chateaubriand/personal-assignment?class=${classNum}`);
 };
 export const getAssignmentTeam = (classNum: number) => {
-  return getApiDefault().get<Team>(`/admin/team-assignment?class=${classNum}`);
+  return getApiDefault().get<Team>(`/chateaubriand/team-assignment?class=${classNum}`);
 };
 export const getAssignmentExperiment = (classNum: number) => {
-  return getApiDefault().get<Experiment>(`/admin/experiment-assignment?class=${classNum}`);
+  return getApiDefault().get<Experiment>(`/chateaubriand/experiment-assignment?class=${classNum}`);
 };
 
 export const downloadAssignmentFiles = (assignmentId: number, assignmentType: string) => {
@@ -31,7 +31,7 @@ export const tokenReIssuance = async () => {
   try {
     const { data } = await axios({
       method: 'POST',
-      url: `${process.env.TEST_BASE_URL}/admin/token`,
+      url: `${process.env.BAST_URL}/chateaubriand/token`,
       data: {
         access_token: localStorage.getItem('accessToken'),
         refresh_token: localStorage.getItem('refreshToken'),
@@ -39,10 +39,9 @@ export const tokenReIssuance = async () => {
     });
     localStorage.setItem('accessToken', data.access_token);
   } catch (err) {
-    console.log(err);
     const code = err?.response?.status;
     if (code === 403) {
-      useHistory().push('/admin/login');
+      useHistory().push('/chateaubriand/login');
     }
   }
 };
