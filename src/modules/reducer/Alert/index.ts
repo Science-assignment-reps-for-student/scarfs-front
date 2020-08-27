24512c7773143671c0b0b0f3fc3175f5ce2badac
이성진 <chlskatnr1@naver.com>
2020-08-27 21:21:44 +0900

    [MODIFY] <AlertModal> 확인 및 취소 callback 함수 전달

--- src/modules/reducer/Alert/index.ts
+++ src/modules/reducer/Alert/index.ts
@@ -2,6 +2,8 @@ const CREATE_ALERT = 'CREATE_ALERT' as const;
 const DELETE_ALERT = 'DELETE_ALERT' as const;
 const RETURN_VALUE = 'RETURN_VALUE' as const;
 const RESET_VALUE = 'RESET_VALUE' as const;
+const SET_CHECK_CALLBACK = 'SET_CHECK_CALLBACK' as const;
+const SET_CANCEL_CALLBACK = 'SET_CANCEL_CALLBACK' as const;
 
 export const createAlert = (explain: string) => ({
   type: CREATE_ALERT,
@@ -17,12 +19,22 @@ export const setReturnValue = (val: boolean) => ({
 export const resetReturnValue = () => ({
   type: RESET_VALUE,
 });
+export const setCheckCallback = (callback: () => void) => ({
+  type: SET_CHECK_CALLBACK,
+  payload: { callback },
+});
+export const setCancelCallback = (callback: () => void) => ({
+  type: SET_CANCEL_CALLBACK,
+  payload: { callback },
+});
 
 type AlertAction =
   | ReturnType<typeof createAlert>
   | ReturnType<typeof deleteAlert>
   | ReturnType<typeof resetReturnValue>
-  | ReturnType<typeof setReturnValue>;
+  | ReturnType<typeof setReturnValue>
+  | ReturnType<typeof setCheckCallback>
+  | ReturnType<typeof setCancelCallback>;
 
 export type AlertModalTypes = 'notify' | 'warn';
 
@@ -30,12 +42,16 @@ export type AlertState = {
   isShow: boolean;
   returnValue: boolean | undefined;
   explain: string;
+  checkCallback: () => void;
+  cancelCallback: () => void;
 };
 
 const initialPersonal: AlertState = {
   isShow: false,
   returnValue: undefined,
   explain: '오류가 발생하였습니다. \n 잠시 후 다시 시도해주세요.',
+  checkCallback: () => {},
+  cancelCallback: () => {},
 };
 
 const alert = (state = initialPersonal, action: AlertAction): AlertState => {
@@ -61,6 +77,16 @@ const alert = (state = initialPersonal, action: AlertAction): AlertState => {
       return {
         ...state,
         returnValue: undefined,
+      };
+    case SET_CHECK_CALLBACK:
+      return {
+        ...state,
+        checkCallback: action.payload.callback,
+      };
+    case SET_CANCEL_CALLBACK:
+      return {
+        ...state,
+        cancelCallback: action.payload.callback,
       };
     default:
       return state;
