551081da7773622c6ee417d56804996a2f068d4e
_uchanlee <uchanlee.dev@gmail.com>
2020-08-28 21:39:01 +0900

    [ADD] <modules/reducer/ClassBoardWrite/index.ts>
    classNumber

--- src/modules/reducer/ClassBoardWrite/index.ts
+++ src/modules/reducer/ClassBoardWrite/index.ts
@@ -4,6 +4,7 @@ export const WRITE_BOARD = 'ClassBoardWrite/WRITE_BOARD' as const;
 export const WRITE_BOARD_SUCCESS = 'ClassBoardWrite/WRITE_BOARD_SUCCESS' as const;
 export const WRITE_BOARD_FAILURE = 'ClassBoardWrite/WRITE_BOARD_FAILURE' as const;
 export const WRITE_BOARD_RESET = 'ClassBoardWrite/WRITE_BOARD_RESET' as const;
+export const WRITE_BOARD_CLASS_NUMBER = 'ClassBoardWrite/WRITE_BOARD_CLASS_NUMBER' as const;
 
 export const writeBoardSuccess = (payload: number) => ({
   type: WRITE_BOARD_SUCCESS,
@@ -19,19 +20,27 @@ export const writeBoardReset = () => ({
   type: WRITE_BOARD_RESET,
 });
 
+export const setWriteBoardClassNumber = (payload: number) => ({
+  type: WRITE_BOARD_CLASS_NUMBER,
+  payload,
+});
+
 export type ClassBoardWriterAction =
   | ReturnType<typeof writeBoardSuccess>
   | ReturnType<typeof writeBoardFailure>
-  | ReturnType<typeof writeBoardReset>;
+  | ReturnType<typeof writeBoardReset>
+  | ReturnType<typeof setWriteBoardClassNumber>;
 
 export type ClassBoardWriteState = {
   writeBoardSuccess: number;
   writeBoardError: ErrorType;
+  classNumber: number;
 };
 
 const initialState: ClassBoardWriteState = {
   writeBoardSuccess: 0,
   writeBoardError: errorInitialState,
+  classNumber: 1,
 };
 
 export default function ClassBoardWriter(
@@ -52,6 +61,11 @@ export default function ClassBoardWriter(
       };
     case WRITE_BOARD_RESET:
       return initialState;
+    case WRITE_BOARD_CLASS_NUMBER:
+      return {
+        ...state,
+        classNumber: action.payload,
+      };
     default:
       return state;
   }
