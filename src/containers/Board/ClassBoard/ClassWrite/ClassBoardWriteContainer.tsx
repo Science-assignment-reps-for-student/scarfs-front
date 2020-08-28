caccee54c1d923de3d8635068da70f80f28a6286
_uchanlee <uchanlee.dev@gmail.com>
2020-08-28 19:00:24 +0900

    [ADD] <containers/Board/~/ClassBoardWriteContainer.tsx>
    connect redux

--- src/containers/Board/ClassBoard/ClassWrite/ClassBoardWriteContainer.tsx
+++ src/containers/Board/ClassBoard/ClassWrite/ClassBoardWriteContainer.tsx
@@ -1,10 +1,44 @@
 import React, { FC } from 'react';
 import { ClassBoardWrite, AlertModal } from '../../../../components';
+import { useSelector, useDispatch } from 'react-redux';
+import { getStateCallback } from '../../../../lib/function';
+import { ClassBoardWriteState, writeBoardReset } from '../../../../modules/reducer/ClassBoardWrite';
+import { writeBoardThunk } from '../../../../modules/thunk/ClassBoardWrite';
+import { LoadingState } from '../../../../modules/reducer/Loading';
+import HeaderState from '../../../../modules/reducer/Header';
+import { Redirect } from 'react-router-dom';
 
 const ClassBoardWriteContainer: FC = () => {
+  const dispatch = useDispatch();
+  const { writeBoardSuccess, writeBoardError } = useSelector(
+    getStateCallback<ClassBoardWriteState>('ClassBoardWrite'),
+  );
+  const { 'Header/GET_USER_INFO': getUserLoading } = useSelector(
+    getStateCallback<LoadingState>('Loading'),
+  );
+  const { userInfo } = useSelector(getStateCallback<HeaderState>('Header'));
+
+  const writeBoard = (data: FormData) => {
+    dispatch(writeBoardThunk(data));
+  };
+
+  const resetWriteBoard = () => {
+    dispatch(writeBoardReset());
+  };
+
+  if (userInfo && userInfo.type === 'STUDENT') {
+    alert('학생은 글쓰기 페이지에 접근할 수 없습니다.');
+    return <Redirect to='/error' />;
+  }
+
   return (
     <AlertModal type='notify'>
-      <ClassBoardWrite />
+      <ClassBoardWrite
+        writeBoard={writeBoard}
+        resetWriteBoard={resetWriteBoard}
+        writeBoardSuccess={writeBoardSuccess}
+        writeBoardError={writeBoardError}
+      />
     </AlertModal>
   );
 };
