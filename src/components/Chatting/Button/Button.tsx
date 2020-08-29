01cdb1b50a6b4a1748f325f07a50584901a0edc0
ohjunsang <pandati@dsm.hs.kr>
2020-08-29 20:38:25 +0900

    [ADD] 안읽은 메세지 있으면 색깔 바뀌게 함

--- src/components/Chatting/Button/Button.tsx
+++ src/components/Chatting/Button/Button.tsx
@@ -3,13 +3,16 @@ import * as S from '../style';
 
 interface Props {
   isAbleChange: (isAble: boolean) => void;
+  alarmChange: (alaram: boolean) => void;
+  alarm: boolean;
 }
 
-const ChattingOpenButton: FC<Props> = ({ isAbleChange }) => {
+const ChattingOpenButton: FC<Props> = ({ isAbleChange, alarmChange, alarm }) => {
   const chattingOpen = useCallback(() => {
     isAbleChange(true);
+    alarmChange(false);
   }, []);
-  return <S.ChattingOpenButton onClick={chattingOpen} />;
+  return <S.ChattingOpenButton onClick={chattingOpen} alarm={alarm} />;
 };
 
 export default ChattingOpenButton;
