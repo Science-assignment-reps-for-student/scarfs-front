523f5cd9bdb30e52bbbd7925fb216408a65112d3
_uchanlee <uchanlee.dev@gmail.com>
2020-09-01 20:26:58 +0900

    [ADD] <modules/thunk/Comment/index.ts> updateCommentThunk

--- src/modules/thunk/Comment/index.ts
+++ src/modules/thunk/Comment/index.ts
@@ -1,5 +1,6 @@
 import { createRequestThunk } from '../../../lib/thunk';
-import { ADD_COMMENT } from '../../reducer/Comment';
-import { addCommnt } from '../../../lib/api/Comment';
+import { ADD_COMMENT, UPDATE_COMMENT } from '../../reducer/Comment';
+import { addCommnt, updateComment } from '../../../lib/api/Comment';
 
 export const addCommentThunk = createRequestThunk(ADD_COMMENT, addCommnt);
+export const updateCommentThunk = createRequestThunk(UPDATE_COMMENT, updateComment);
