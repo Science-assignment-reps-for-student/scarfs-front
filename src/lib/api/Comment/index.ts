439bdb3fcf98902f9991f94f578505e4aa318b96
_uchanlee <uchanlee.dev@gmail.com>
2020-09-01 20:27:50 +0900

    [ADD] <lib/api/Comment/index.ts> updateComment api

--- src/lib/api/Comment/index.ts
+++ src/lib/api/Comment/index.ts
@@ -4,3 +4,6 @@ export const addCommnt = ({ boardId, content }: { boardId: number; content: stri
   getApiDefault().post(`/shank/comment/${boardId}`, {
     content,
   });
+
+export const updateComment = ({ commentId, content }) =>
+  getApiDefault().put<number>(`/shank/comment/${commentId}`, { content });
