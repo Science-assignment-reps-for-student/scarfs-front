70ac7d10b6acceb20d7ec478a2b7acdf56d18f07
_uchanlee <uchanlee.dev@gmail.com>
2020-09-02 15:21:05 +0900

    [ADD] <container/~/CommentModal/index.ts> useAddReCommentRedux

--- src/containers/Board/DetailPost/ClassDetailPost/CommentModal/index.ts
+++ src/containers/Board/DetailPost/ClassDetailPost/CommentModal/index.ts
@@ -1,1 +1,28 @@
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getStateCallback } from '../../../../../lib/function';
+import { addReCommentThunk } from '../../../../../modules/thunk/Comment';
+import { CommentState } from '../../../../../modules/reducer/Comment';
+import { ErrorType } from '../../../../../lib/type';
+
 export { default as CommentModalContainer } from './CommentModalContainer';
+
+export const useAddReCommentRedux = (): [
+  number,
+  ErrorType,
+  (commentId: number, content: string) => void,
+] => {
+  const dispatch = useDispatch();
+  const { addReCommentSuccess, addReCommentError } = useSelector(
+    getStateCallback<CommentState>('Comment'),
+  );
+
+  const addReComment = useCallback(
+    (commentId: number, content: string) => {
+      dispatch(addReCommentThunk({ commentId, content }));
+    },
+    [dispatch],
+  );
+
+  return [addReCommentSuccess, addReCommentError, addReComment];
+};
