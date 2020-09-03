f837d6885f93710235fb9ffe2165ea4b73aa3db0
_uchanlee <uchanlee.dev@gmail.com>
2020-09-03 20:48:00 +0900

    [UPDATE] <containers/~/ClassDetailPostContainer.tsx> connect container

--- src/containers/Board/DetailPost/ClassDetailPost/ClassDetailPostContainer.tsx
+++ src/containers/Board/DetailPost/ClassDetailPost/ClassDetailPostContainer.tsx
@@ -1,17 +1,23 @@
 import React, { FC } from 'react';
-import { ClassDetailPost, AlertModal } from '../../../../components';
+import { ClassDetailPost } from '../../../../components';
+// import { ClassDetailPost, AlertModal } from '../../../../components';
 import { useDispatch, useSelector } from 'react-redux';
-import { getDetailPostThunk } from '../../../../modules/thunk/ClassDetailPost';
+import {
+  getDetailPostThunk,
+  deleteClassDetailPostThunk,
+} from '../../../../modules/thunk/ClassDetailPost';
 import { getStateCallback } from '../../../../lib/function';
 import { ClassDetailPostState, resetDetailPost } from '../../../../modules/reducer/ClassDetailPost';
 import { LoadingState } from 'src/modules/reducer/Loading';
 
 const ClassDetailPostContainer: FC = () => {
   const dispatch = useDispatch();
-
-  const { classDetailPost, getDetailPostError } = useSelector(
-    getStateCallback<ClassDetailPostState>('ClassDetailPost'),
-  );
+  const {
+    classDetailPost,
+    getDetailPostError,
+    deleteDetailPostSuccess,
+    deleteDetailPostError,
+  } = useSelector(getStateCallback<ClassDetailPostState>('ClassDetailPost'));
 
   const { 'ClassDetailPost/GET_DETAIL_POST': isLoading } = useSelector(
     getStateCallback<LoadingState>('Loading'),
@@ -21,20 +27,27 @@ const ClassDetailPostContainer: FC = () => {
     dispatch(getDetailPostThunk(boardId));
   };
 
+  const deleteDetailPost = (boardId: number) => {
+    dispatch(deleteClassDetailPostThunk(boardId));
+  };
+
   const resetDetailPostHandler = () => {
     dispatch(resetDetailPost());
   };
 
   return (
-    <AlertModal type='notify'>
-      <ClassDetailPost
-        isLoading={isLoading}
-        classDetailPost={classDetailPost}
-        getDetailPostError={getDetailPostError}
-        getDetailPost={getDetailPost}
-        resetDetailPost={resetDetailPostHandler}
-      />
-    </AlertModal>
+    // <AlertModal type='notify'>
+    <ClassDetailPost
+      isLoading={isLoading}
+      classDetailPost={classDetailPost}
+      getDetailPostError={getDetailPostError}
+      getDetailPost={getDetailPost}
+      deleteDetailPost={deleteDetailPost}
+      deleteDetailPostSuccess={deleteDetailPostSuccess}
+      deleteDetailPostError={deleteDetailPostError}
+      resetDetailPost={resetDetailPostHandler}
+    />
+    // </AlertModal>
   );
 };
 
