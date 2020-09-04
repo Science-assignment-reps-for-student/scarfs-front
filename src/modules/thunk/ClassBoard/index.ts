541c221e8680b1020dbc66fd3960d8c37d33c6b4
_uchanlee <uchanlee.dev@gmail.com>
2020-09-05 00:44:55 +0900

    [ADD] <modules/thunk/ClassBoard/index.ts> searchClassBoardThunk

--- src/modules/thunk/ClassBoard/index.ts
+++ src/modules/thunk/ClassBoard/index.ts
@@ -1,5 +1,6 @@
 import { createRequestThunk } from '../.././../lib/thunk';
-import { GET_BOARD } from '../../reducer/ClassBoard';
-import { getClassBoard } from '../../../lib/api/ClassBoard';
+import { GET_BOARD, SEARCH_CLASS_BOARD } from '../../reducer/ClassBoard';
+import { getClassBoard, searchClassBoard } from '../../../lib/api/ClassBoard';
 
 export const getBoardThunk = createRequestThunk(GET_BOARD, getClassBoard);
+export const searchClassBoardThunk = createRequestThunk(SEARCH_CLASS_BOARD, searchClassBoard);
