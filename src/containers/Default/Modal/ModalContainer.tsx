28858b7ce21301eafcb2dc8d556465a21c146315
_uchanlee <uchanlee.dev@gmail.com>
2020-09-05 20:25:25 +0900

    [UPDATE] <containers/~/ModalContainer.tsx> FileSubmitModalContainer

--- src/containers/Default/Modal/ModalContainer.tsx
+++ src/containers/Default/Modal/ModalContainer.tsx
@@ -1,12 +1,8 @@
 import React, { FC } from 'react';
 import { useSelector } from 'react-redux';
 import { SignUpCode, SignUpInfo, SignIn } from '../../../components/Default/Modal';
-import {
-  FileSubmitModal,
-  PeerEvaluationModal,
-  AddTeamMemberModal,
-} from '../../../components/Board/DetailPost';
-import { CommentModalContainer } from '../../Board/DetailPost';
+import { PeerEvaluationModal, AddTeamMemberModal } from '../../../components/Board/DetailPost';
+import { CommentModalContainer, FileSubmitModalContainer } from '../../Board/DetailPost';
 import { ModalType, ModalState } from '../../../modules/reducer/Modal';
 import { getStateCallback } from '../../../lib/function';
 
@@ -18,7 +14,7 @@ const ModalContainer: FC = () => {
       {(modal as ModalType) === 'SignUpEmail' && <SignUpCode />}
       {(modal as ModalType) === 'SignUpInfo' && <SignUpInfo />}
       {(modal as ModalType) === 'SignIn' && <SignIn />}
-      {(modal as ModalType) === 'FileSubmit' && <FileSubmitModal />}
+      {(modal as ModalType) === 'FileSubmit' && <FileSubmitModalContainer />}
       {(modal as ModalType) === 'PeerEvaluation' && <PeerEvaluationModal />}
       {(modal as ModalType) === 'AddTeamMember' && <AddTeamMemberModal />}
       {(modal as ModalType) === 'CommentModal' && <CommentModalContainer />}
