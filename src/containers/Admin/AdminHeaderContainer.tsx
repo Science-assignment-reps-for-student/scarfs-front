79ce1b075e4a81b23dfab72e6bc96aa45aee4e3d
이성진 <chlskatnr1@naver.com>
2020-09-06 23:38:23 +0900

    [MODIFY] <AdminHeaderContainer> 메인 페이지로 이동 시 redux token 변경

--- src/containers/Admin/AdminHeaderContainer.tsx
+++ src/containers/Admin/AdminHeaderContainer.tsx
@@ -2,7 +2,7 @@ import React, { FC, ReactElement, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { AdminHeader } from '../../components';
-import { setAccessToken, setIsLogin } from '../../modules/reducer/Header';
+import { setAccessToken, setRefreshToken, setIsLogin } from '../../modules/reducer/Header';
 interface Props {}
 
 const AdminHeaderContainer: FC<Props> = (): ReactElement => {
@@ -11,6 +11,7 @@ const AdminHeaderContainer: FC<Props> = (): ReactElement => {
   useEffect(() => {
     () => {
       dispatch(setAccessToken(localStorage.getItem('accessToken')));
+      dispatch(setRefreshToken(localStorage.getItem('refreshToken')));
       dispatch(setIsLogin(true));
     };
   }, []);
