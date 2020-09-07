f5e9ea8b57916a09ac69139b2fa9f5b27707ec7c
ohjunsang <pandati@dsm.hs.kr>
2020-09-07 19:28:38 +0900

    [ADD] boardTaskList Id 없어서 추가

--- src/components/Main/Login/TaskList/BoardTaskList.tsx
+++ src/components/Main/Login/TaskList/BoardTaskList.tsx
@@ -19,7 +19,12 @@ const BoardTaskList: FC<Props> = ({ boardPreview, isLoading }) => {
     const buffer = [];
     boardPreview.applicationResponses.map(board => {
       buffer.push(
-        <BoardTaskListComponent date={board.created_at} title={board.title} id={board.notice_id} />,
+        <BoardTaskListComponent
+          key={'board' + board.notice_id}
+          date={board.created_at}
+          title={board.title}
+          id={board.notice_id}
+        />,
       );
     });
     return buffer;
