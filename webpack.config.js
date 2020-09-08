7be120596e39ade680f1399663d76db0b8ef7706
_uchanlee <uchanlee.dev@gmail.com>
2020-09-08 11:22:46 +0900

    [UPDATE] <webpack.config.js> require module

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
