3f0fa1e3780085e5dfed5c93c71ffae6f6c25718
이성진 <chlskatnr1@naver.com>
2020-09-08 15:29:02 +0900

    [MODIFY] <AdminMain/SubmitList> Props 오류 해결

--- src/components/Admin/AdminMain/SubmitList.tsx
+++ src/components/Admin/AdminMain/SubmitList.tsx
@@ -3,15 +3,16 @@ import * as S from './style';
 import { submitted, unSubmitted } from '../../../assets/Admin';
 
 interface Props {
-  studentId: string;
+  student_id: number;
+  student_number: string;
   name: string;
   submit: number;
 }
 
-const SubmitList: FC<Props> = ({ studentId, name, submit }): ReactElement => {
+const SubmitList: FC<Props> = ({ student_number, name, submit, student_id }): ReactElement => {
   return (
     <S.SubjectClsContentCommonItem className={submit ? 'submitted' : 'unSubmitted'}>
-      <S.SubjectClsContentCommonItemText>{studentId}</S.SubjectClsContentCommonItemText>
+      <S.SubjectClsContentCommonItemText>{student_number}</S.SubjectClsContentCommonItemText>
       <S.SubjectClsContentCommonItemText>{name}</S.SubjectClsContentCommonItemText>
       <S.SubjectClsContentCommonItemText>
         {<img src={submit ? submitted : unSubmitted} alt='condition' title='condition' />}
