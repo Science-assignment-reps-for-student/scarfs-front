ac048ee13cd8573cac0a60b91cfb084106df1048
_uchanlee <uchanlee.dev@gmail.com>
2020-09-09 15:51:51 +0900

    [UPDATE] <webpack.config.js> bundle_[hash].min.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'bundle.min.js',
+    filename: 'bundle_[hash].min.js',
     publicPath: '/',
   },
   module: {
