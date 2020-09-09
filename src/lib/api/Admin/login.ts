7c6c14510f0d94ea25e7c5f0424f1a45a6a49c2d
이성진 <chlskatnr1@naver.com>
2020-09-09 18:19:36 +0900

    [MODIFY] <api/Admin> admin api 타입 지정

--- src/lib/api/Admin/login.ts
+++ src/lib/api/Admin/login.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+interface Login {
+  access_token: string;
+  refresh_token: string;
+}
+
 export const apiLogin = ({ ID, PW }: { ID: string; PW: string }) => {
-  return axios.post(`${process.env.BASE_URL}/chateaubriand/auth`, {
+  return axios.post<Login>(`${process.env.BASE_URL}/chateaubriand/auth`, {
     email: ID,
     password: PW,
   });
--- src/lib/api/Admin/signUp.ts
+++ src/lib/api/Admin/signUp.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
+
 import { SignUp } from '../../../modules/reducer/AdminSignUp/reducer';
 
 export const apiSignUp = ({ email, password, name }: SignUp) => {
-  return axios.post(`${process.env.BASE_URL}/chateaubriand/account`, {
+  return axios.post<{}>(`${process.env.BASE_URL}/chateaubriand/account`, {
     email: email,
     password: password,
     name: name,
