d9e4ea7d9d90a8af01f3c37963e80026c0e88ffd
_uchanlee <uchanlee.dev@gmail.com>
2020-09-11 08:36:33 +0900

    [ADD] <containers/~/DetailPost/**> export

--- src/containers/Board/DetailPost/AssignmentDetailPost/index.ts
+++ src/containers/Board/DetailPost/AssignmentDetailPost/index.ts
@@ -1,3 +1,4 @@
 export { default as AssignmentDetailPostContainer } from './AssignmentDetailPostContainer';
 export { EvaluationContainer } from './Evaluation';
 export { FileSubmitModalContainer } from './FileSubmitModal';
+export { CreateTeamModalContainer } from './CreateTeamModal';
--- src/containers/Board/DetailPost/index.ts
+++ src/containers/Board/DetailPost/index.ts
@@ -2,6 +2,7 @@ export {
   AssignmentDetailPostContainer,
   EvaluationContainer,
   FileSubmitModalContainer,
+  CreateTeamModalContainer,
 } from './AssignmentDetailPost';
 export { ClassDetailPostContainer, CommentModalContainer } from './ClassDetailPost';
 export { NoticeDetailPostContainer } from './NoticeDetailPost';
