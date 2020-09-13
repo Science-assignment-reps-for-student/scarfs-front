1fbad32a103df81b92d40b16c9f5369dd1ef7379
_uchanlee <uchanlee.dev@gmail.com>
2020-09-13 22:57:26 +0900

    [ADD] <modules/AddTeamMember/index.ts> deleteTeamMemberThunk

--- src/modules/thunk/AddTeamMember/index.ts
+++ src/modules/thunk/AddTeamMember/index.ts
@@ -1,5 +1,6 @@
 import { createRequestThunk } from '../../../lib/thunk';
-import { GET_STUDENTS } from '../../reducer/AddTeamMember';
-import { getStudents } from '../../../lib/api/AddTeamMember';
+import { GET_STUDENTS, DELETE_TEAM_MEMBER } from '../../reducer/AddTeamMember';
+import { getStudents, deleteTeamMember } from '../../../lib/api/AddTeamMember';
 
 export const getStudentsThunk = createRequestThunk(GET_STUDENTS, getStudents);
+export const deleteTeamMemberThunk = createRequestThunk(DELETE_TEAM_MEMBER, deleteTeamMember);
