b298cfc2d0172ec104b3836c70f9d3d6dcf16564
이성진 <chlskatnr1@naver.com>
2020-09-15 21:49:45 +0900

    [MODIFY] <api/Evaluation> 평가 api 추가

--- src/lib/api/Evaluation/index.ts
+++ src/lib/api/Evaluation/index.ts
@@ -7,6 +7,41 @@ export interface SelfEvaluation {
   attitude: number;
 }
 
+export interface PeerEvaluation {
+  assignment_id: number;
+  target_id: number;
+  cooperation: number;
+  communication: number;
+}
+
+export interface AlreadySelfEvaluation {
+  scientific_accuracy: number;
+  communication: number;
+  attitude: number;
+  createdAt: string;
+}
+
+export interface TargetEvaluation {
+  cooperation: number;
+  communication: number;
+}
+
 export const apiSelfEvaluation = (self: SelfEvaluation) => {
   return getApiDefault().post<{}>('/shank/evaluation/personal', self);
+};
+
+export const apiPeerEvaluation = (peer: PeerEvaluation) => {
+  return getApiDefault().post<{}>(`/shank/evaluation/team`, peer);
+};
+
+export const apiAlreadySelfEvaluationInfo = (assignmentId: string) => {
+  return getApiDefault().get<AlreadySelfEvaluation>(
+    `/shank/evaluation/info/personal/${assignmentId}`,
+  );
+};
+
+export const apiTargetEvaluation = (assignmentId: string, targetId: string) => {
+  return getApiDefault().get<TargetEvaluation>(
+    `shank/evaluation/info/team/${assignmentId}?targetId=${targetId}`,
+  );
 };
