4fd85f79d59e6960318b2a59fed69ee8b656d958
이성진 <chlskatnr1@naver.com>
2020-09-15 21:46:30 +0900

    [MODIFY] <EvaluationContainer> 평가 리스트 가져오는 api 추가

--- src/containers/Board/DetailPost/AssignmentDetailPost/Evaluation/EvaluationContainer.tsx
+++ src/containers/Board/DetailPost/AssignmentDetailPost/Evaluation/EvaluationContainer.tsx
@@ -1,10 +1,44 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
 import { Evaluation } from '../../../../../components';
+import { setPeers } from '../../../../../modules/reducer/Evaluation';
+import { reducerType } from '../../../../../modules/reducer';
+import { apiPeerEvaluation } from '../../../../../lib/api/PeerEvaluationModal';
+import { tokenReIssuance } from '../../../../../lib/api/Admin/admin';
 
 const EvaluationContainer: FC = () => {
-  const submitSelf = () => console.log('self');
-  const submitMutal = () => console.log('mutal');
-  return <Evaluation submitSelf={submitSelf} submitMutal={submitMutal} />;
+  const dispatch = useDispatch();
+  const { peers } = useSelector((state: reducerType) => state.Evaluation);
+  const params = useParams<{ id: string }>();
+  const homeworkId = params.id;
+
+  const handlePeers = async () => {
+    const { data } = await apiPeerEvaluation(homeworkId);
+    dispatch(setPeers(data));
+  };
+
+  const getPeers = async () => {
+    try {
+      await handlePeers();
+    } catch (err) {
+      const code = err?.response?.status;
+      if (!code) return;
+      if (code === 401) {
+        await tokenReIssuance();
+        await handlePeers();
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (peers.length === 0) {
+      getPeers();
+    }
+  }, [peers]);
+
+  return <Evaluation />;
 };
 
 export default EvaluationContainer;
