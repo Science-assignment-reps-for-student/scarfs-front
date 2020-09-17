fceb6bb373d12c48c545cf8d38a8bc8def5d426c
이성진 <chlskatnr1@naver.com>
2020-09-17 12:58:42 +0900

    [MODIFY] <Admin/WithSubject>  code style

--- src/components/Admin/AdminMain/WithSubject.tsx
+++ src/components/Admin/AdminMain/WithSubject.tsx
@@ -1,9 +1,11 @@
 import React, { FC, useMemo } from 'react';
-import { CombineAdminSubjects, CombineAdminSubject } from '../../../modules/reducer/Admin';
+
 import Subject from './Subject';
 import Team from './Team/Team';
 import Personal from './Personal/Personal';
 import Experiment from './Experiment/Experiment';
+
+import { CombineAdminSubjects, CombineAdminSubject } from '../../../modules/reducer/Admin';
 
 interface Filter {
   class1: boolean;
@@ -14,33 +16,38 @@ interface Filter {
   team: boolean;
   experiment: boolean;
 }
+
 interface CombineResult {
   id: number;
   classes: CombineAdminSubjects;
 }
-interface Props {
-  subject: CombineResult;
-  filter: Filter;
-}
+
 interface MyComponentProps {
   subject: CombineAdminSubject;
   filter: Filter;
   classNum: number;
 }
 
+interface Props {
+  subject: CombineResult;
+  filter: Filter;
+}
+
 const WithSubjectComponent = (MyComponent: FC<MyComponentProps>) => (props: Props) => {
   const { subject, filter } = props;
 
-  const getClasses = useMemo(() => {
-    return subject.classes.map((subject: CombineAdminSubject, i) => (
-      <MyComponent
-        key={Object.keys(filter)[i]}
-        subject={subject}
-        filter={filter}
-        classNum={i + 1}
-      />
-    ));
-  }, [subject]);
+  const getClasses = useMemo(
+    () =>
+      subject.classes.map((subject: CombineAdminSubject, i) => (
+        <MyComponent
+          key={Object.keys(filter)[i]}
+          subject={subject}
+          filter={filter}
+          classNum={i + 1}
+        />
+      )),
+    [subject],
+  );
 
   return <Subject subject={subject.classes}>{getClasses}</Subject>;
 };
